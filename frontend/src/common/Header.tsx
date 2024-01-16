279f60396bc7c1dce92db2598c7a8ef3f77e5b53
KIMHYEONJI <hyeonji0718@gmail.com>
2024-01-16 14:42:12 +0900

    ✨ feat : Header에 signUp 페이지 이동 버튼 추가

--- frontend/src/common/Header.tsx
+++ frontend/src/common/Header.tsx
@@ -1,5 +1,12 @@
+import { Link } from 'react-router-dom'
+
 const Header = () => {
-  return <header>헤더 영역</header>
+  return (
+    <header>
+      헤더 영역
+      <Link to="/signUp">회원가입</Link>
+    </header>
+  )
 }
 
 export default Header
