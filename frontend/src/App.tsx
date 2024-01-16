fe3f68183c3ccf1278a9b83bdc54e9b3506319ce
KIMHYEONJI <hyeonji0718@gmail.com>
2024-01-16 14:31:15 +0900

    ✨ feat : signUp Route 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,9 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import HomePage from '@src/pages/HomePage'
 import LoginPage from '@src/pages/LoginPage'
+import SignUpPage from '@src/pages/SignUpPage'
 import Header from '@src/common/Header'
+
 // 로그인된 경우 접근할 수 있는 url
 const AuthRoutes = () => (
   <Routes>
@@ -16,6 +18,7 @@ const HomeRoutes = () => (
   <Routes>
     <Route path="/" element={<HomePage />} />
     <Route path="/login" element={<LoginPage />} />
+    <Route path="/signUp" element={<SignUpPage />} />
   </Routes>
 )
 
