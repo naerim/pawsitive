2d579799743a387d45540f880be4d7658d65b1fb
KIMHYEONJI <hyeonji0718@gmail.com>
2024-01-16 14:31:35 +0900

    ✨ feat : signUp skeleton 생성

---
+++ frontend/src/container/SignUpContainer.tsx
@@ -0,0 +1,5 @@
+const SignUpContainer = () => {
+  return <h2>NEW ACCOUNT</h2>
+}
+
+export default SignUpContainer
---
+++ frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,7 @@
+import SignUpContainer from '@src/container/SignUpContainer'
+
+const SignUpPage = () => {
+  return <SignUpContainer />
+}
+
+export default SignUpPage
