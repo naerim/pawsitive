b6ef263197e0578dc3416de791f0ddd10e913a7a
KIMHYEONJI <hyeonji0718@gmail.com>
2024-01-16 15:24:26 +0900

    ✨ feat : 이름 입력 field 생성
    - 이름이 2-20자가 아닐 경우 경고 문구 표시

--- frontend/src/container/SignUpContainer.tsx
+++ frontend/src/container/SignUpContainer.tsx
@@ -1,5 +1,35 @@
+import React, { useState } from 'react'
+
 const SignUpContainer = () => {
-  return <h2>NEW ACCOUNT</h2>
+  const [name, setName] = useState('')
+  const [nameError, setNameError] = useState('')
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const nameInput = e.target.value
+    setName(nameInput)
+
+    if (nameInput.length < 2 || nameInput.length > 20) {
+      setNameError('실명을 입력해주세요.')
+    } else setNameError('')
+  }
+  return (
+    <>
+      <h2>NEW ACCOUNT</h2>
+      <div>
+        <label htmlFor="name">
+          이름
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
+        </label>
+        <div>{nameError}</div>
+      </div>
+    </>
+  )
 }
 
 export default SignUpContainer
