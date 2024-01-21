b7da6b993ae7d12d7b49c7cecb02f8e09b42ced6
김내림 <naerim1119@naver.com>
2024-01-22 00:19:04 +0900

    ⚙️ config : pwa setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,45 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-});
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: { enabled: true },
+      manifest: {
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/maskable_icon_x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'any',
+          },
+          {
+            src: '/maskable_icon_x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'maskable',
+          },
+          {
+            src: '/maskable_icon.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'any',
+          },
+          {
+            src: '/maskable_icon.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
+})
