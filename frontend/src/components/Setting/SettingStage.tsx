c215273d75203ca9d8e46c54eca4647deecd9183
김내림 <naerim1119@naver.com>
2024-01-23 15:32:19 +0900

    ✨ feat : 사용자 stage 바꾸는 컴포넌트 생성

---
+++ frontend/src/components/Setting/SettingStage.tsx
@@ -0,0 +1,12 @@
+const SettingStage = () => {
+  return (
+    <div>
+      <button type="button">stage 0</button>
+      <button type="button">stage 1</button>
+      <button type="button">stage 2</button>
+      <button type="button">stage 3</button>
+    </div>
+  )
+}
+
+export default SettingStage
--- frontend/src/container/SettingContainer.tsx
+++ frontend/src/container/SettingContainer.tsx
@@ -1,9 +1,11 @@
 import ThemeToggle from '@src/components/Setting/ThemeToggle'
+import SettingStage from '@src/components/Setting/SettingStage'
 
 const SettingContainer = () => {
   return (
     <div>
       <ThemeToggle />
+      <SettingStage />
     </div>
   )
 }
