3089cfb097abf8b04d1f3893d892be015f539074
김내림 <naerim1119@naver.com>
2024-01-23 16:04:37 +0900

    ✨ feat : 사용자 stage 변경 함수 구현

--- frontend/src/components/Setting/SettingStage.tsx
+++ frontend/src/components/Setting/SettingStage.tsx
@@ -1,10 +1,26 @@
+import { useSetAtom } from 'jotai'
+import { userAtom } from '@src/stores/atoms/user'
+
 const SettingStage = () => {
+  const setUser = useSetAtom(userAtom)
+
+  const changeUserStage = (num: number) =>
+    setUser(user => ({ ...user, stage: num }))
+
   return (
     <div>
-      <button type="button">stage 0</button>
-      <button type="button">stage 1</button>
-      <button type="button">stage 2</button>
-      <button type="button">stage 3</button>
+      <button type="button" onClick={() => changeUserStage(0)}>
+        stage 0
+      </button>
+      <button type="button" onClick={() => changeUserStage(1)}>
+        stage 1
+      </button>
+      <button type="button" onClick={() => changeUserStage(2)}>
+        stage 2
+      </button>
+      <button type="button" onClick={() => changeUserStage(3)}>
+        stage 3
+      </button>
     </div>
   )
 }
