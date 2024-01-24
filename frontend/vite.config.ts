681dd9de2a35140f09a75b81dd41c1d3f77f6612
최성호 <chltjdgh3@naver.com>
2024-01-24 17:42:49 +0900

    🌱 chore : vite에서 .env 식별하도록 코드 수정

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,55 +1,59 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tsconfigPaths(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: { enabled: true },
-      manifest: {
-        name: 'pawsitive',
-        short_name: 'pawsitive',
-        start_url: '',
-        display: 'standalone',
-        orientation: 'portrait',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: '/maskable_icon_x192.png',
-            type: 'image/png',
-            sizes: '192x192',
-            purpose: 'any',
-          },
-          {
-            src: '/maskable_icon_x192.png',
-            type: 'image/png',
-            sizes: '192x192',
-            purpose: 'maskable',
-          },
-          {
-            src: '/maskable_icon.png',
-            type: 'image/png',
-            sizes: '512x512',
-            purpose: 'any',
-          },
-          {
-            src: '/maskable_icon.png',
-            type: 'image/png',
-            sizes: '512x512',
-            purpose: 'maskable',
-          },
-        ],
-      },
-    }),
-  ],
-  server: {
-    port: 3000,
-    host: true,
-    origin: 'http://0.0.0.0',
-  },
+export default defineConfig(mode => {
+  const env = loadEnv(mode, process.cwd())
+  // console.log(env)
+  return {
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        devOptions: { enabled: true },
+        manifest: {
+          name: 'pawsitive',
+          short_name: 'pawsitive',
+          start_url: '',
+          display: 'standalone',
+          orientation: 'portrait',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: '/maskable_icon_x192.png',
+              type: 'image/png',
+              sizes: '192x192',
+              purpose: 'any',
+            },
+            {
+              src: '/maskable_icon_x192.png',
+              type: 'image/png',
+              sizes: '192x192',
+              purpose: 'maskable',
+            },
+            {
+              src: '/maskable_icon.png',
+              type: 'image/png',
+              sizes: '512x512',
+              purpose: 'any',
+            },
+            {
+              src: '/maskable_icon.png',
+              type: 'image/png',
+              sizes: '512x512',
+              purpose: 'maskable',
+            },
+          ],
+        },
+      }),
+    ],
+    server: {
+      port: 3000,
+      host: true,
+      origin: 'http://0.0.0.0',
+    },
+  }
 })
