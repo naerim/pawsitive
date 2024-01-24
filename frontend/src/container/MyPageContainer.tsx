815694c84af58cadc1a2c588cbe767ba59bb2580
eestingz <tmdgus1761@gmail.com>
2024-01-25 02:32:36 +0900

    ✨ feat : teachable-machine 구현

--- frontend/src/container/MyPageContainer.tsx
+++ frontend/src/container/MyPageContainer.tsx
@@ -1,9 +1,10 @@
 import MyPageMenu from '@src/components/MyPage/MyPageMenu'
-
+import TeachableMachine from '@src/components/MyPage/TeachableMachine.jsx'
 const MyPageContainer = () => {
   return (
     <div>
       <MyPageMenu />
+      <TeachableMachine />
     </div>
   )
 }
