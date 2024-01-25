aed520983a6896d9e32fe6d5be490d1050a01d84
김내림 <naerim1119@naver.com>
2024-01-25 17:44:59 +0900

    🌱 chore : BASEURL 변경

--- frontend/src/container/CreateDogContainer.tsx
+++ frontend/src/container/CreateDogContainer.tsx
@@ -12,19 +12,49 @@ const CreateDogContainer = () => {
       <div>공고등록일</div>
       <input type="text" />
       <div>중성화여부</div>
-      <input type="radio" id="naturalized" name="naturalized" value="1" />
+      <input type="radio" id="naturalizedo" name="naturalized" value="1" />
       중성화 o
-      <input type="radio" id="naturalized" name="naturalized" value="0" />
+      <input type="radio" id="naturalizedx" name="naturalized" value="0" />
       중성화 x<div>색상</div>
       <input type="text" />
-      <div>유기견 추가 정보</div>
+      <div>유기견 추가 정보 ESAF QIWC</div>
       <div>
-        <input type="radio" id="e1" name="e" value="0" />
-        <label htmlFor="e1">질문</label>
+        <input type="radio" id="e1" name="e" value="300" />
+        <label htmlFor="e1">잘 때 빼고 항상 활발하고 밝아요.</label>
       </div>
       <div>
-        <input type="radio" id="e2" name="e" value="300" />
-        <label htmlFor="e2">질문</label>
+        <input type="radio" id="e2" name="e" value="0" />
+        <label htmlFor="e2">집에서도 스스로 잘 놀아요</label>
+      </div>
+      <div>
+        <input type="radio" id="s1" name="s" value="300" />
+        <label htmlFor="e1">반려인을 많이 따라요</label>
+      </div>
+      <div>
+        <input type="radio" id="s2" name="s" value="0" />
+        <label htmlFor="e2">스스로 행동하는 걸 선호해요</label>
+      </div>
+      <div>
+        <input type="radio" id="a1" name="a" value="300" />
+        <label htmlFor="e1">
+          모든 것에 호기심이 많고 즐거운 성격을 가지고 있어요
+        </label>
+      </div>
+      <div>
+        <input type="radio" id="a2" name="a" value="0" />
+        <label htmlFor="e2">
+          낯가리는 성향으로 처음 보는 환경에는 조심스럽게 대응해요
+        </label>
+      </div>
+      <div>
+        <input type="radio" id="f1" name="f" value="300" />
+        <label htmlFor="e1">
+          성실하게 주인의 요구에 따르며 언제나 충성스럽게 행동해요.
+        </label>
+      </div>
+      <div>
+        <input type="radio" id="f2" name="f" value="0" />
+        <label htmlFor="e2">재치 있고 조금은 개성 넘치게 행동해요</label>
       </div>
       <div />
       <Camera />
--- frontend/src/hooks/requestMethods.ts
+++ frontend/src/hooks/requestMethods.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://i10c111.p.ssafy.io:50001'
+const BASE_URL = 'http://i10c111.p.ssafy.io:8081'
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
