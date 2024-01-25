6af03a93f42a8a9351f6ec6daef8e8055c112ff4
이승현 <tmdgus1761@gmail.com>
2024-01-25 14:00:13 +0900

    ✨ feat : teachable 컨테이너 추가

---
+++ frontend/src/container/TeachableContainer.tsx
@@ -0,0 +1,11 @@
+import TeachableMachine from '@src/components/Teachable/TeachableMachine'
+
+const TeachableContainer = () => {
+  return (
+    <div>
+      <TeachableMachine />
+    </div>
+  )
+}
+
+export default TeachableContainer
