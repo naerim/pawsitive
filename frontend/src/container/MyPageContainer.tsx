22dc96e4a2bbe5e286f8919ae20220c2ad24b98c
이승현 <tmdgus1761@gmail.com>
2024-01-25 13:58:55 +0900

    ✨ feat : teachable-machine 컴포넌트 경로 삭제

--- frontend/src/container/MyPageContainer.tsx
+++ frontend/src/container/MyPageContainer.tsx
@@ -1,11 +1,9 @@
 import MyPageMenu from '@src/components/MyPage/MyPageMenu'
-import TeachableMachine from '@src/components/MyPage/TeachableMachine.tsx'
 
 const MyPageContainer = () => {
   return (
     <div>
       <MyPageMenu />
-      <TeachableMachine />
     </div>
   )
 }
