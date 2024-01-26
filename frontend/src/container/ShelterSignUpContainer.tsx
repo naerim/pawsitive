d66fa48b55db7e281d71b0fb63fcf515fdac2b41
KIMHYEONJI <hyeonji0718@gmail.com>
2024-01-26 12:07:03 +0900

    ✨ feat : 주소 검색 결과 buildingName이 있으면 나타나도록 설정

--- frontend/src/container/ShelterSignUpContainer.tsx
+++ frontend/src/container/ShelterSignUpContainer.tsx
@@ -5,37 +5,46 @@ import { DaumPostData } from '@src/types/container/SignUpType'
 const ShelterSignUpContainer = () => {
   const [address, setAddress] = useState('')
   const [isDaumPostcodeOpen, setIsDaumPostcodeOpen] = useState(false)
+  const [buildingName, setBuilidngName] = useState('')
+
   const handleDaumPostcodeOpen = () => {
     setIsDaumPostcodeOpen(true)
   }
 
   const handleAddressComplete = (data: DaumPostData) => {
     setAddress(data.address)
+    setBuilidngName(data.buildingName)
     setIsDaumPostcodeOpen(false)
   }
 
   return (
-    <div>
-      <label htmlFor="address">
-        주소
-        {isDaumPostcodeOpen && (
-          <div>
-            <button type="button" onClick={() => setIsDaumPostcodeOpen(false)}>
-              닫기
-            </button>
-            <DaumPostcode
-              onComplete={handleAddressComplete}
-              style={{ position: 'absolute', zIndex: 100, width: 200 }}
-            />
-          </div>
-        )}
-        <input
-          placeholder="주소를 검색해주세요"
-          onClick={handleDaumPostcodeOpen}
-          defaultValue={address}
-        />
-      </label>
-    </div>
+    <>
+      <div>
+        <label htmlFor="address">
+          주소
+          {isDaumPostcodeOpen && (
+            <div>
+              <button
+                type="button"
+                onClick={() => setIsDaumPostcodeOpen(false)}
+              >
+                닫기
+              </button>
+              <DaumPostcode
+                onComplete={handleAddressComplete}
+                style={{ position: 'absolute', zIndex: 100, width: 200 }}
+              />
+            </div>
+          )}
+          <input
+            placeholder="주소를 검색해주세요"
+            onClick={handleDaumPostcodeOpen}
+            defaultValue={address}
+          />
+        </label>
+      </div>
+      <div>{buildingName && <input defaultValue={buildingName} />}</div>
+    </>
   )
 }
 
