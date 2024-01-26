bdfd4694e7bcc4e84ff0f34de56536065ae0965c
최성호 <chltjdgh3@naver.com>
2024-01-26 15:36:51 +0900

    ✨ feat : adoption survey 컨테이너 추가

---
+++ frontend/src/container/AdoptionSurveyContainer.tsx
@@ -0,0 +1,11 @@
+import AdoptionSurveyInfo from '@src/components/AdoptionSurvey/AdoptionSurveyInfo'
+
+const AdoptionSurveyContainer = () => {
+  return (
+    <div>
+      <AdoptionSurveyInfo />
+    </div>
+  )
+}
+
+export default AdoptionSurveyContainer
