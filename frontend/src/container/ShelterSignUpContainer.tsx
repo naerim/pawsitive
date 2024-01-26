eee83cf1893c87ca429344732d9841fda87259d8
KIMHYEONJI <hyeonji0718@gmail.com>
2024-01-26 12:05:11 +0900

    ✨ feat : 보호소 주소 입력 field 생성

--- frontend/src/container/ShelterSignUpContainer.tsx
+++ frontend/src/container/ShelterSignUpContainer.tsx
@@ -1,5 +1,42 @@
+import { useState } from 'react'
+import DaumPostcode from 'react-daum-postcode'
+import { DaumPostData } from '@src/types/container/SignUpType'
+
 const ShelterSignUpContainer = () => {
-  return <div />
+  const [address, setAddress] = useState('')
+  const [isDaumPostcodeOpen, setIsDaumPostcodeOpen] = useState(false)
+  const handleDaumPostcodeOpen = () => {
+    setIsDaumPostcodeOpen(true)
+  }
+
+  const handleAddressComplete = (data: DaumPostData) => {
+    setAddress(data.address)
+    setIsDaumPostcodeOpen(false)
+  }
+
+  return (
+    <div>
+      <label htmlFor="address">
+        주소
+        {isDaumPostcodeOpen && (
+          <div>
+            <button type="button" onClick={() => setIsDaumPostcodeOpen(false)}>
+              닫기
+            </button>
+            <DaumPostcode
+              onComplete={handleAddressComplete}
+              style={{ position: 'absolute', zIndex: 100, width: 200 }}
+            />
+          </div>
+        )}
+        <input
+          placeholder="주소를 검색해주세요"
+          onClick={handleDaumPostcodeOpen}
+          defaultValue={address}
+        />
+      </label>
+    </div>
+  )
 }
 
 export default ShelterSignUpContainer
