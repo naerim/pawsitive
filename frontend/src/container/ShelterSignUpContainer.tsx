b36450a7f4e9f66d1379e8d920679f1cbe442334
KIMHYEONJI <hyeonji0718@gmail.com>
2024-01-26 11:33:47 +0900

    ✨ feat : ShelterSignUp skeleton 생성

---
+++ frontend/src/container/ShelterSignUpContainer.tsx
@@ -0,0 +1,5 @@
+const ShelterSignUpContainer = () => {
+  return <div />
+}
+
+export default ShelterSignUpContainer
---
+++ frontend/src/pages/ShelterSignUpPage.tsx
@@ -0,0 +1,7 @@
+import ShelterSignUpContainer from '@src/container/ShelterSignUpContainer'
+
+const ShelterSignUpPage = () => {
+  return <ShelterSignUpContainer />
+}
+
+export default ShelterSignUpPage
