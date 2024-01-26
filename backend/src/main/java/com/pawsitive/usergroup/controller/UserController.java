e15e0f44eaaa8816ee31a932059abdf02f1eafce
이하늬 <109622@naver.com>
2024-01-26 11:32:42 +0900

    🐛 fix : 서버 오류 해결
    entity 수정으로 인한 jpaRepository 메서드이름으로쿼리생성 해결

--- backend/src/main/java/com/pawsitive/usergroup/controller/UserController.java
+++ backend/src/main/java/com/pawsitive/usergroup/controller/UserController.java
@@ -1,46 +1,29 @@
-//package com.pawsitive.usergroup.controller;
-//
-//import static org.springframework.http.HttpStatus.CREATED;
-//import static org.springframework.http.HttpStatus.NO_CONTENT;
-//import static org.springframework.http.HttpStatus.OK;
-//
-//import com.pawsitive.auth.CustomUserDetails;
-//import com.pawsitive.common.dto.BaseResponseBody;
-//import com.pawsitive.usergroup.dto.request.UserRegisterPostReq;
-//import com.pawsitive.usergroup.dto.request.UserUpdatePatchReq;
-//import com.pawsitive.usergroup.dto.response.AdoptedDogRes;
-//import com.pawsitive.usergroup.dto.response.UserCheckRes;
-//import com.pawsitive.usergroup.dto.response.UserRes;
-//import com.pawsitive.usergroup.dto.response.UserUpdateRes;
-//import com.pawsitive.usergroup.entity.User;
-//import com.pawsitive.usergroup.exception.DuplicateIdException;
-//import com.pawsitive.usergroup.exception.UserNotLoginException;
-//import com.pawsitive.usergroup.service.UserService;
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.responses.ApiResponse;
-//import io.swagger.v3.oas.annotations.tags.Tag;
-//import java.util.Objects;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PatchMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-///**
-// * 유저 관련 API 요청 처리를 위한 컨트롤러 정의.
-// */
-//@Tag(name = "03.User")
-//@RestController
-//@RequestMapping("/api/v1/users")
-//@RequiredArgsConstructor
-//public class UserController {
-//
+package com.pawsitive.usergroup.controller;
+
+import static org.springframework.http.HttpStatus.OK;
+
+import com.pawsitive.usergroup.dto.response.AdoptedDogRes;
+import com.pawsitive.usergroup.exception.UserNotLoginException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 유저 관련 API 요청 처리를 위한 컨트롤러 정의.
+ */
+@Tag(name = "03.User")
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
 //    private final UserService userService;
 //
 //    @PostMapping
@@ -115,34 +98,34 @@
 //            .status(OK)
 //            .build();
 //    }
-//
-//    @GetMapping("/{userId}/dogs")
-//    @Operation(summary = "로그인 한 회원의 입양한 유기견 조회",
-//        description = "<strong>로그인 한 회원이 입양한 유기견</strong>을 조회한다.",
-//        tags = {"03.User"},
-//        responses = {
-//            @ApiResponse(responseCode = "200", description = "로그인 한 회원의 입양한 유기견 조회에 성공하였습니다."),
-//            @ApiResponse(responseCode = "401", description = "현재 로그인 한 회원의 계정이 유효하지 않습니다."),
-//        }
-//    )
-//    public ResponseEntity<AdoptedDogRes> getDogsByUser(@PathVariable String userId,
-//                                                       Authentication authentication) {
-//
-//        if (!userId.equals("admin")) {
-//            throw new UserNotLoginException();
-//        }
-//
-//        AdoptedDogRes response = AdoptedDogRes.builder()
-//            .adoptedDays(290)
-//            .answerCount(10)
-//            .memoryCount(9)
-//            .build();
-//
-//        return ResponseEntity
-//            .status(OK)
-//            .body(response);
-//    }
-//
+
+    @GetMapping("/{userId}/dogs")
+    @Operation(summary = "로그인 한 회원의 입양한 유기견 조회",
+        description = "<strong>로그인 한 회원이 입양한 유기견</strong>을 조회한다.",
+        tags = {"03.User"},
+        responses = {
+            @ApiResponse(responseCode = "200", description = "로그인 한 회원의 입양한 유기견 조회에 성공하였습니다."),
+            @ApiResponse(responseCode = "401", description = "현재 로그인 한 회원의 계정이 유효하지 않습니다."),
+        }
+    )
+    public ResponseEntity<AdoptedDogRes> getDogsByUser(@PathVariable String userId,
+                                                       Authentication authentication) {
+
+        if (!userId.equals("admin")) {
+            throw new UserNotLoginException();
+        }
+
+        AdoptedDogRes response = AdoptedDogRes.builder()
+            .adoptedDays(290)
+            .answerCount(10)
+            .memoryCount(9)
+            .build();
+
+        return ResponseEntity
+            .status(OK)
+            .body(response);
+    }
+
 //    @PatchMapping("/{userId}")
 //    public ResponseEntity<UserUpdateRes> modifyUser(@PathVariable String userId,
 //                                                    @RequestBody UserUpdatePatchReq updateInfo,
@@ -174,5 +157,5 @@
 //            .status(NO_CONTENT)
 //            .body(UserCheckRes.of(NO_CONTENT, "Success"));
 //    }
-//
-//}
+
+}
