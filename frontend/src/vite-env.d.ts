cce69027188c8b8eec192ad6d407ed25c1138521
choiseongho <chltjdgh3@naver.com>
2024-01-28 19:17:41 +0900

    🌱 chore : vite .env 설정 수정

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_APP_TITLE: string
+  readonly VITE_APP_KAKAO_MAP_API_KEY: string
   // 다른 환경 변수들에 대한 타입 정의...
 }
 
