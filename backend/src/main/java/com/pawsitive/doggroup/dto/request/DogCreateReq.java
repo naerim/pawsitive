f90394e527038c2a7a2ca8cae3ace118d276ee74
이하늬 <109622@naver.com>
2024-01-28 15:05:32 +0900

    ✨ feat : 유기견 등록 시 요청 데이터 유효성 검증 로직 추가

--- backend/src/main/java/com/pawsitive/doggroup/dto/request/DogCreateReq.java
+++ backend/src/main/java/com/pawsitive/doggroup/dto/request/DogCreateReq.java
@@ -1,8 +1,9 @@
 package com.pawsitive.doggroup.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.web.multipart.MultipartFile;
+import org.hibernate.validator.constraints.Length;
 
 /**
  * @author 이하늬
@@ -11,17 +12,34 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @AllArgsConstructor
 public class DogCreateReq {
+    @NotNull(message = "userNo can't be null")
     private int userNo;
+
+    @NotNull(message = "name can't be null")
     private String name;
+
+    @NotNull(message = "kind can't be null")
     private String kind;
+
+    @NotNull(message = "isNaturalized can't be null")
     private Boolean isNaturalized;
+
+    @NotNull(message = "color can't be null")
     private String color;
+
+    @Length(max = 500)
     private String note;
-    private int eq;
-    private int si;
-    private int aw;
-    private int fc;
-    private MultipartFile video;
-    private MultipartFile[] photoList;
+
+    @NotNull(message = "eq can't be null")
+    private Boolean eq;
+
+    @NotNull(message = "si can't be null")
+    private Boolean si;
+
+    @NotNull(message = "aw can't be null")
+    private Boolean aw;
+
+    @NotNull(message = "fc can't be null")
+    private Boolean fc;
 
 }
