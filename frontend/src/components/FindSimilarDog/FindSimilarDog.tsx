abc948c4f97dabe732ac5bca3a6b5d0d2ce0361d
이승현 <tmdgus1761@gmail.com>
2024-01-29 12:02:59 +0900

    ✨ feat : 티처블 머신 학습 모델 수정

--- frontend/src/components/FindSimilarDog/FindSimilarDog.tsx
+++ frontend/src/components/FindSimilarDog/FindSimilarDog.tsx
@@ -1,94 +1,7 @@
-import * as tmImage from '@teachablemachine/image'
-
-const FindSimilarDog = () => {
-  const URL = 'https://teachablemachine.withgoogle.com/models/KDq_2gk72/'
-
-  let model
-  let webcam
-  let labelContainer
-  let maxPredictions
-
-  let isPredicting = false
-
-  async function init() {
-    if (isPredicting) {
-      return // If already predicting, do nothing
-    }
-
-    isPredicting = true
-
-    // Clear previous webcam instance if exists
-    stop()
-
-    const modelURL = `${URL}model.json`
-    const metadataURL = `${URL}metadata.json`
-
-    model = await tmImage.load(modelURL, metadataURL)
-    maxPredictions = model.getTotalClasses()
-
-    const flip = true
-    webcam = new tmImage.Webcam(200, 200, flip)
-    await webcam.setup()
-    await webcam.play()
-    window.requestAnimationFrame(loop)
-
-    // Clear previous webcam container content
-    document.getElementById('webcam-container').innerHTML = ''
-
-    // Append new webcam canvas
-    document.getElementById('webcam-container').appendChild(webcam.canvas)
-
-    labelContainer = document.getElementById('label-container')
-    for (let i = 0; i < maxPredictions; i++) {
-      labelContainer.appendChild(document.createElement('div'))
-    }
-  }
-
-  function stop() {
-    if (webcam) {
-      webcam.stop()
-      isPredicting = false
-    }
-  }
-
-  async function loop() {
-    if (isPredicting) {
-      webcam.update() / (await predict())
-      window.requestAnimationFrame(loop)
-    }
-  }
-
-  async function predict() {
-    const prediction = await model.predict(webcam.canvas)
-    for (let i = 0; i < maxPredictions; i++) {
-      const classPrediction = `${prediction[i].className}:  ${Math.round(
-        prediction[i].probability * 100,
-      )}%`
-      labelContainer.childNodes[i].innerHTML = classPrediction
-    }
-  }
-
-  return (
-    <div>
-      <div>Teachable Machine Image Model</div>
-      <button type="button" onClick={init}>
-        Start
-      </button>
-      <button type="button" onClick={stop}>
-        Stop
-      </button>
-      <div id="webcam-container" />
-      <div id="label-container" />
-    </div>
-  )
-}
-
-export default FindSimilarDog
-
 // import * as tmImage from '@teachablemachine/image'
 //
 // const FindSimilarDog = () => {
-//   const URL = 'https://teachablemachine.withgoogle.com/models/KDq_2gk72/'
+//   const URL = 'https://teachablemachine.withgoogle.com/models/WEqtMay2T/'
 //
 //   let model
 //   let webcam
@@ -116,31 +29,182 @@ export default FindSimilarDog
 //   }
 //
 //   async function loop() {
-//     webcam.update() / (await predict())
+//     webcam.update()
+//     await predict()
 //     window.requestAnimationFrame(loop)
 //   }
 //
 //   async function predict() {
 //     const prediction = await model.predict(webcam.canvas)
 //     for (let i = 0; i < maxPredictions; i++) {
-//       console.log(prediction)
-//       console.log(typeof prediction)
-//       const classPrediction = `${prediction[i].className}:  ${Math.round(prediction[i].probability * 100)}%`
+//       const classPrediction = `${prediction[i].className}: ${prediction[i].probability.toFixed(2)}`
 //       labelContainer.childNodes[i].innerHTML = classPrediction
 //     }
 //   }
 //
 //   return (
 //     <div>
-//       <div> FindSimilarDog Machine Image Model</div>
+//       <div>Teachable Machine Image Model</div>
 //       <button type="button" onClick={init}>
 //         Start
 //       </button>
 //       <div id="webcam-container" />
 //       <div id="label-container" />
-//       {/* <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest/dist/tf.min.js" /> */}
-//       {/* <script src="https://cdn.jsdelivr.net/npm/@teachablemachine/image@latest/dist/teachablemachine-image.min.js" /> */}
 //     </div>
 //   )
 // }
+//
 // export default FindSimilarDog
+//
+// import * as tmImage from '@teachablemachine/image'
+//
+// const FindSimilarDog = () => {
+//   const URL = 'https://teachablemachine.withgoogle.com/models/WEqtMay2T/'
+//
+//   let model
+//   let webcam
+//   let labelContainer
+//   let maxPredictions
+//
+//   async function init() {
+//     const modelURL = `${URL}model.json`
+//     const metadataURL = `${URL}metadata.json`
+//
+//     // 이전 웹캠 인스턴스가 존재하면 제거
+//     stop()
+//
+//     model = await tmImage.load(modelURL, metadataURL)
+//     maxPredictions = model.getTotalClasses()
+//
+//     const flip = true
+//     webcam = new tmImage.Webcam(200, 200, flip)
+//     await webcam.setup()
+//     await webcam.play()
+//     window.requestAnimationFrame(loop)
+//
+//     // 이전 웹캠 캔버스를 지우고 새로운 웹캠 캔버스 추가
+//     document.getElementById('webcam-container').innerHTML = ''
+//     document.getElementById('webcam-container').appendChild(webcam.canvas)
+//
+//     labelContainer = document.getElementById('label-container')
+//     for (let i = 0; i < maxPredictions; i++) {
+//       labelContainer.appendChild(document.createElement('div'))
+//     }
+//   }
+//
+//   function stop() {
+//     if (webcam) {
+//       webcam.stop()
+//     }
+//   }
+//
+//   async function loop() {
+//     webcam.update()
+//     await predict()
+//     window.requestAnimationFrame(loop)
+//   }
+//
+//   async function predict() {
+//     const prediction = await model.predict(webcam.canvas)
+//     for (let i = 0; i < maxPredictions; i++) {
+//       const classPrediction = `${prediction[i].className}: ${Math.round(prediction[i].probability * 100)}%`
+//       labelContainer.childNodes[i].innerHTML = classPrediction
+//     }
+//   }
+//
+//   return (
+//     <div>
+//       <div>Teachable Machine Image Model</div>
+//       <button type="button" onClick={init}>
+//         Start
+//       </button>
+//       <button type="button" onClick={stop}>
+//         Stop
+//       </button>
+//       <div id="webcam-container" />
+//       <div id="label-container" />
+//     </div>
+//   )
+// }
+//
+// export default FindSimilarDog
+
+import * as tmImage from '@teachablemachine/image'
+
+const FindSimilarDog = () => {
+  const URL = 'https://teachablemachine.withgoogle.com/models/WEqtMay2T/'
+
+  let model
+  let webcam
+  let labelContainer
+  let maxPredictions
+
+  let isPredicting = false
+
+  async function init() {
+    // 이전 웹캠 인스턴스가 존재하면 제거
+    stop()
+
+    const modelURL = `${URL}model.json`
+    const metadataURL = `${URL}metadata.json`
+
+    model = await tmImage.load(modelURL, metadataURL)
+    maxPredictions = model.getTotalClasses()
+
+    const flip = true
+    webcam = new tmImage.Webcam(200, 200, flip)
+    await webcam.setup()
+    await webcam.play()
+    window.requestAnimationFrame(loop)
+
+    // 이전 웹캠 캔버스를 지우고 새로운 웹캠 캔버스 추가
+    document.getElementById('webcam-container').innerHTML = ''
+    document.getElementById('webcam-container').appendChild(webcam.canvas)
+
+    labelContainer = document.getElementById('label-container')
+    for (let i = 0; i < maxPredictions; i++) {
+      labelContainer.appendChild(document.createElement('div'))
+    }
+
+    isPredicting = true
+  }
+
+  function stop() {
+    if (webcam) {
+      webcam.stop()
+      isPredicting = false
+    }
+  }
+
+  async function loop() {
+    if (isPredicting) {
+      webcam.update()
+      await predict()
+      window.requestAnimationFrame(loop)
+    }
+  }
+
+  async function predict() {
+    const prediction = await model.predict(webcam.canvas)
+    for (let i = 0; i < maxPredictions; i++) {
+      const classPrediction = `${prediction[i].className}: ${Math.round(prediction[i].probability * 100)}%`
+      labelContainer.childNodes[i].innerHTML = classPrediction
+    }
+  }
+
+  return (
+    <div>
+      <div>Teachable Machine Image Model</div>
+      <button type="button" onClick={init}>
+        Start
+      </button>
+      <button type="button" onClick={stop}>
+        Stop
+      </button>
+      <div id="webcam-container" />
+      <div id="label-container" />
+    </div>
+  )
+}
+
+export default FindSimilarDog
