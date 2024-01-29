9f5887e142b7b7d525cb836900103aee6865d261
choiseongho <chltjdgh3@naver.com>
2024-01-30 00:44:10 +0900

    ♻️ refactor : kakao map 참조 위치 변경

--- frontend/src/container/CommunityCreateContainer.tsx
+++ frontend/src/container/CommunityCreateContainer.tsx
@@ -1,11 +1,9 @@
 import CreateForm from '@src/components/Community/CreateForm'
-import KakaoMap from '@src/components/Community/KakaoMap'
 
 const CommunityCreateContainer = () => {
   return (
     <div>
       <CreateForm />
-      <KakaoMap dummyData={[]} />
     </div>
   )
 }
