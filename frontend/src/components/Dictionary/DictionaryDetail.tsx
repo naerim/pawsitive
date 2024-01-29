202b3757a53d3c4b25c0ae18079aa1046c8abaef
이승현 <tmdgus1761@gmail.com>
2024-01-30 01:52:08 +0900

    🎨 style : 펫과사전 상세 조회 페이지 기초 css 적용

--- frontend/src/components/Dictionary/DictionaryDetail.tsx
+++ frontend/src/components/Dictionary/DictionaryDetail.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as c from '@src/components/style/DictionaryDetail'
 import { DictionaryItemType } from '@src/types/components/DictionaryType'
 
 interface DictionaryDetailProps {
@@ -13,46 +14,62 @@ const DictionaryDetail: React.FC<DictionaryDetailProps> = ({ item }) => {
   switch (item.contentCategoryName) {
     case '펫티켓':
       contentRenderer = (
-        <div>
-          {item.image && <img src={item.image} alt="" />}
-          <h2>{item.title}</h2>
-          <p>정보 : {item.content && JSON.parse(item.content).description}</p>
-          <p>부가 정보 : {item.content && JSON.parse(item.content).remarks}</p>
-        </div>
+        <c.DetailContainer>
+          {item.image && <c.DetailImg src={item.image} alt="" />}
+          <c.DetailTitle>{item.title}</c.DetailTitle>
+          <c.DetailInfo>
+            정보 : {item.content && JSON.parse(item.content).description}
+          </c.DetailInfo>
+          <c.DetailInfo>
+            부가 정보 : {item.content && JSON.parse(item.content).remarks}
+          </c.DetailInfo>
+        </c.DetailContainer>
       )
       break
     case '건강':
       contentRenderer = (
-        <div>
-          {item.image && <img src={item.image} alt="" />}
-          <h2>{item.title}</h2>
-          <p>정보 : {item.content && JSON.parse(item.content).description}</p>
-          <p>증상 : {item.content && JSON.parse(item.content).symptom}</p>
-          <p>예방 : {item.content && JSON.parse(item.content).prevent}</p>
-        </div>
+        <c.DetailContainer>
+          {item.image && <c.DetailImg src={item.image} alt="" />}
+          <c.DetailTitle>{item.title}</c.DetailTitle>
+          <c.DetailInfo>
+            정보 : {item.content && JSON.parse(item.content).description}
+          </c.DetailInfo>
+          <c.DetailInfo>
+            증상 : {item.content && JSON.parse(item.content).symptom}
+          </c.DetailInfo>
+          <c.DetailInfo>
+            예방 : {item.content && JSON.parse(item.content).prevent}
+          </c.DetailInfo>
+        </c.DetailContainer>
       )
       break
     case '상식':
       contentRenderer = (
-        <div>
-          {item.image && <img src={item.image} alt="" />}
-          <h2>{item.title}</h2>
-          <p>정보 : {item.content && JSON.parse(item.content).description}</p>
-        </div>
+        <c.DetailContainer>
+          {item.image && <c.DetailImg src={item.image} alt="" />}
+          <c.DetailTitle>{item.title}</c.DetailTitle>
+          <c.DetailInfo>
+            정보 : {item.content && JSON.parse(item.content).description}
+          </c.DetailInfo>
+        </c.DetailContainer>
       )
       break
     case '기타':
       contentRenderer = (
-        <div>
-          {item.image && <img src={item.image} alt="" />}
-          <h2>{item.title}</h2>
-          <p>정보 : {item.content && JSON.parse(item.content).description}</p>
-        </div>
+        <c.DetailContainer>
+          {item.image && <c.DetailImg src={item.image} alt="" />}
+          <c.DetailTitle>{item.title}</c.DetailTitle>
+          <c.DetailInfo>
+            정보 : {item.content && JSON.parse(item.content).description}
+          </c.DetailInfo>
+        </c.DetailContainer>
       )
       break
 
     default:
-      contentRenderer = <div>알 수 없는 카테고리</div>
+      contentRenderer = (
+        <c.DetailUnknownCategory>알 수 없는 카테고리</c.DetailUnknownCategory>
+      )
       break
   }
 
