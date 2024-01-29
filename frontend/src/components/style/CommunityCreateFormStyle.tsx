fc77bafe8cbea0dc1a81d53678e93184e9d7f202
choiseongho <chltjdgh3@naver.com>
2024-01-30 00:46:07 +0900

    🎨 style : 커뮤니티 글 생성 페이지 css 추가

--- frontend/src/components/style/CommunityCreateFormStyle.tsx
+++ frontend/src/components/style/CommunityCreateFormStyle.tsx
@@ -3,9 +3,22 @@ import styled from 'styled-components'
 export const Container = styled.div``
 export const Label = styled.label`
   background-color: darkseagreen;
+  border-radius: 5px;
 `
 
 export const ImageInput = styled.input`
   opacity: 0;
 `
 export const ImagePreview = styled.div``
+
+export const Map = styled.div`
+  width: 300px;
+  height: 300px;
+  border: 2px solid gray;
+  border-radius: 40px;
+`
+export const Input = styled.input`
+  width: 300px;
+  height: 25px;
+  border-radius: 10px;
+`
