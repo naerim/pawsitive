5d14ce232471ed1d4a3ada3b467c61e2bafeaf97
이승현 <tmdgus1761@gmail.com>
2024-01-30 00:29:25 +0900

    ✨ feat : 펫과사전 상세 조회 skeleton 코드 작성

---
+++ frontend/src/components/Dictionary/DictionaryDetail.tsx
@@ -0,0 +1,4 @@
+const DictionaryDetail = () => {
+  return <div>펫과사전 디테일</div>
+}
+export default DictionaryDetail
---
+++ frontend/src/container/DictionaryDetailContainer.tsx
@@ -0,0 +1,6 @@
+import DictionaryDetail from '@src/components/Dictionary/DictionaryDetail'
+
+const DictionaryDetailContainer = () => {
+  return <DictionaryDetail />
+}
+export default DictionaryDetailContainer
