72b4f046b6c7801a75b6fe691945061e6f17a0a5
KIMHYEONJI <hyeonji0718@gmail.com>
2024-01-30 21:27:45 +0900

    ♻️ refactor : 코드의 가독성을 위해 Index 컴포넌트 import 이름 변경
    - Index -> ThirdStage

--- frontend/src/container/HomeContainer.tsx
+++ frontend/src/container/HomeContainer.tsx
@@ -5,7 +5,7 @@ import FirstStage from '@src/components/Home/FirstStage'
 import HomeLayout from '@src/components/Home/HomeLayout'
 import HomePopularCommunity from '@src/components/Home/HomePopularCommunity'
 import SecondStage from '@src/components/Home/SecondStage'
-import Index from '@src/components/Home/ThirdStage'
+import ThirdStage from '@src/components/Home/ThirdStage'
 
 const HomeContainer = () => {
   const user = useAtomValue(userAtom)
@@ -23,7 +23,7 @@ const HomeContainer = () => {
       currentStageComponent = <SecondStage />
       break
     case 4:
-      currentStageComponent = <Index />
+      currentStageComponent = <ThirdStage />
       break
     default:
       currentStageComponent = <div />
