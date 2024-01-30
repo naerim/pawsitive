4615f3e9f8f261bb87d827d32fde57510b9eecf2
choiseongho <chltjdgh3@naver.com>
2024-01-31 01:21:22 +0900

    🎨 style : community list css 추가

---
+++ frontend/src/components/style/CommunityListStyle.tsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+
+export const Box = styled.div`
+  margin: 10px;
+  padding: 10px;
+  background-color: #fdf1e8;
+  border-radius: 10px;
+`
+export const Container = styled.div`
+  width: auto;
+  height: 60px;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  padding: 10px;
+  background-color: #fde2ca;
+  border-radius: 10px;
+  color: #ff984f;
+`
+
+export const H1 = styled.h1`
+  font-weight: bolder;
+  font-size: 17px;
+`
+
+export const Col = styled.div`
+  display: grid;
+  grid-template-columns: auto auto;
+  justify-content: space-between;
+`
+
+export const P = styled.p`
+  margin-top: 7px;
+  font-size: 13px;
+  flex: 1;
+`
+
+export const Category = styled.p``
