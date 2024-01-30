698f7b1fde8bf99a4f539649c78c7a555867aad4
KIMHYEONJI <hyeonji0718@gmail.com>
2024-01-30 21:42:43 +0900

    🐛 fix : HomeLayout 두 번 rendering 되는 것 수정

--- frontend/src/components/Home/ThirdStage/index.tsx
+++ frontend/src/components/Home/ThirdStage/index.tsx
@@ -1,15 +1,14 @@
-import HomeLayout from '@src/components/Home/HomeLayout'
 import DogInfo from '@src/components/Home/ThirdStage/DogInfo'
 import PawsitiverCertification from '@src/components/Home/ThirdStage/PawsitiverCertification'
 import AdoptInfo from '@src/components/Home/ThirdStage/AdoptInfo'
 
 const Index = () => {
   return (
-    <HomeLayout>
+    <>
       <AdoptInfo />
       <DogInfo />
       <PawsitiverCertification />
-    </HomeLayout>
+    </>
   )
 }
 
