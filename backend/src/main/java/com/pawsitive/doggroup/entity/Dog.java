0c94e3f8c5ebf7c63c0c1bc4a819aab33bb7fb82
천세진 <sejinnnnnn@naver.com>
2024-01-30 14:47:08 +0900

    🐛 fix : Dog Entity 수정

--- backend/src/main/java/com/pawsitive/doggroup/entity/Dog.java
+++ backend/src/main/java/com/pawsitive/doggroup/entity/Dog.java
@@ -3,6 +3,8 @@ package com.pawsitive.doggroup.entity;
 import com.pawsitive.usergroup.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -25,6 +27,7 @@ public class Dog {
 
     @Id
     @Column(name = "dog_no")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int dogNo;
 
     @ManyToOne
