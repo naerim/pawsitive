f0dbd476a2576c6b180872255080ce0349e8a474
이승현 <tmdgus1761@gmail.com>
2024-01-31 00:36:20 +0900

    ✨ feat : stage1 메인페이지 추천 유기견 컴포넌트 생성

---
+++ frontend/src/components/Home/FirstStage/HomeRecommendDog.tsx
@@ -0,0 +1,22 @@
+import LightColorMoveCard from '@src/common/LightColorMoveCard'
+
+const HomeRecommendDog = () => {
+  return (
+    <div>
+      <div>나와 잘 맞는 강아지</div>
+      <div>저와 함께 행복하게 살아요!</div>
+      <div>
+        <div>
+          <img />
+          <div />
+        </div>
+        <div>
+          <img />
+          <div />
+        </div>
+      </div>
+      <LightColorMoveCard title="더 많은 강아지 찾아보기" url="/" />
+    </div>
+  )
+}
+export default HomeRecommendDog
