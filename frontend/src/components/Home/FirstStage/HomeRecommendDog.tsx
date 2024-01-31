9d90590d23e24df83e262f77cc64602cd3baebf4
이승현 <tmdgus1761@gmail.com>
2024-01-31 17:12:54 +0900

    ✨ feat : stage1 메인페이지 추천 유기견 api 연결

--- frontend/src/components/Home/FirstStage/HomeRecommendDog.tsx
+++ frontend/src/components/Home/FirstStage/HomeRecommendDog.tsx
@@ -1,24 +1,37 @@
 import * as h from '@src/components/style/HomeRecommendDogStyle'
 import LightColorMoveCard from '@src/common/LightColorMoveCard'
+import { useQuery } from '@tanstack/react-query'
+import { fetchRecommendDogs } from '@src/apis/dog'
 
 const HomeRecommendDog = () => {
+  const { data, isLoading } = useQuery({
+    queryKey: ['recommendDog'],
+    queryFn: () => fetchRecommendDogs(2),
+  })
   return (
     <h.Container>
       <h.SubTitle>나와 잘 맞는 강아지</h.SubTitle>
       <h.Title>저와 함께 행복하게 살아요!</h.Title>
       <h.Wrap>
-        <h.Item>
-          <img src="/img/img_dog1.png" alt="" />
-          <h.ItemTitle>충청-00450949</h.ItemTitle>
-          <h.ItemSubTitle>수컷 ∙ 중성화X</h.ItemSubTitle>
-          <h.ItemSubTitle>2021(년생) ∙ 13kg</h.ItemSubTitle>
-        </h.Item>
-        <h.Item>
-          <img src="/img/img_dog2.png" alt="" />
-          <h.ItemTitle>경상-00178394</h.ItemTitle>
-          <h.ItemSubTitle>암컷 ∙ 중성화X</h.ItemSubTitle>
-          <h.ItemSubTitle>2023(년생) ∙ 9kg</h.ItemSubTitle>
-        </h.Item>
+        {!isLoading ? (
+          data &&
+          data.map((item, index) => (
+            <h.Item key={item.id || index}>
+              {item.images && item.images.length > 0 && (
+                <img src={item.images[0]} alt="" />
+              )}
+              <h.ItemTitle>{item.name}</h.ItemTitle>
+              <h.ItemSubTitle>
+                {item.kind}, {item.age}살
+              </h.ItemSubTitle>
+              <h.ItemSubTitle>
+                {item.naturalized ? '중성화O' : '중성화X'}
+              </h.ItemSubTitle>
+            </h.Item>
+          ))
+        ) : (
+          <div>로딩중</div>
+        )}
       </h.Wrap>
       <LightColorMoveCard title="더 많은 강아지 찾아보기" url="/" />
     </h.Container>
