3577b674506f8fd673c6b990535d201168fea2b5
이승현 <tmdgus1761@gmail.com>
2024-01-31 11:43:21 +0900

    🐛 fix : requestMethod conflict 해결

--- frontend/src/hooks/requestMethods.tsx
+++ frontend/src/hooks/requestMethods.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://i10c111.p.ssafy.io'
+const BASE_URL = 'https://i10c111.p.ssafy.io/api/v1'
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
