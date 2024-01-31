1f9615d491fa694b74e1839d0e7613b8be798f6e
최성호 <chltjdgh3@naver.com>
2024-01-31 10:41:29 +0900

    ♻️ refactor : Community list atom 적용

--- frontend/src/container/CommunityListContainer.tsx
+++ frontend/src/container/CommunityListContainer.tsx
@@ -2,6 +2,9 @@ import CommunityList from '@src/components/Community/CommunityList'
 import { CommunityItemType } from '@src/types/components/CommunityType'
 import { useQuery } from '@tanstack/react-query'
 import { fetchCommunityList } from '@src/apis/community'
+import { CommunityListAtom } from '@src/stores/atoms/community'
+import { useAtom } from 'jotai'
+import { useEffect } from 'react'
 
 const CommunityListContainer = () => {
   const { isLoading, data } = useQuery<CommunityItemType[]>({
@@ -9,13 +12,21 @@ const CommunityListContainer = () => {
     queryFn: () => fetchCommunityList(),
   })
 
+  const [CommunityListValue, setCommunityList] = useAtom(CommunityListAtom)
+
+  useEffect(() => {
+    if (data) {
+      setCommunityList(data)
+    }
+  }, [data, setCommunityList])
+
   return (
     <div>
-      {isLoading ? (
+      {isLoading || !CommunityListValue || CommunityListValue.length === 0 ? (
         <p>Loading...</p>
       ) : (
         <div>
-          <CommunityList data={data} />
+          <CommunityList data={CommunityListValue} />
         </div>
       )}
     </div>
