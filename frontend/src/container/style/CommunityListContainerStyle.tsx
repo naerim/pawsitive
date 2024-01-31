ee1689b0892792f19d9eec123ab383362c378b00
최성호 <chltjdgh3@naver.com>
2024-01-31 12:02:40 +0900

    ✨ feat : community category 가로 style 추가

---
+++ frontend/src/container/style/CommunityListContainerStyle.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Category = styled.div`
+  display: flex;
+  overflow-x: auto; /* 가로 스크롤을 허용하도록 설정 */
+  white-space: nowrap; /* 내용물이 한 줄로 유지되도록 설정 */
+`
