3aff0dea6c154470a32f2fcfd3806add9b538dd0
김내림 <naerim1119@naver.com>
2024-01-31 16:56:14 +0900

    🎨 style : Home Community Card 스타일 변경

--- frontend/src/components/Home/SecondStage/index.tsx
+++ frontend/src/components/Home/SecondStage/index.tsx
@@ -1,18 +1,14 @@
-import MainColorMoveCard from '@src/common/MainColorMoveCard'
 import HomeProgressBar from '@src/components/Home/HomeProgressBar'
 import HomeStatistics from '@src/components/Home/HomeStatistics'
 import HomeDictionary from '@src/components/Home/SecondStage/HomeDictionary'
+import HomeCommunityCard from '@src/components/Home/SecondStage/HomeCommunityCard.tsx'
 
 const Index = () => {
   return (
     <div>
       <HomeProgressBar currentStage={2} />
       <HomeStatistics />
-      <MainColorMoveCard
-        title="유기견 입양한 후기 보러가요!"
-        subTitle="포지티버의 후기 확인하기"
-        url="/community "
-      />
+      <HomeCommunityCard />
       <HomeDictionary />
     </div>
   )
---
+++ frontend/src/components/style/HomeCommunityCard.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #ebebeb;
+  width: 86%;
+  margin: 0 auto;
+  padding: 20px 0;
+  border-radius: 10px;
+`
+export const Wrap = styled.div`
+  margin-top: 20px;
+`
