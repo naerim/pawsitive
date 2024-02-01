aae8d661054c891bc9f57ef458a396a20f25b2e4
naerim <naerim1119@gmail.com>
2024-02-02 02:40:39 +0900

    ♻️ refactor : 카테고리마다 이미지 다르게 설정

--- frontend/src/components/CommunityList/CommunityListItem.tsx
+++ frontend/src/components/CommunityList/CommunityListItem.tsx
@@ -1,16 +1,27 @@
 import * as c from '@src/components/style/CommunityListItemStyle'
 import { CommunityItemType } from '@src/types/components/CommunityType'
-import React from 'react'
 
 interface CommunityProps {
   data: CommunityItemType
 }
 
-const CommunityCard: React.FC<CommunityProps> = props => {
+const CommunityCard = (props: CommunityProps) => {
   const { data } = props
+
+  const setImageSrc = (category: string) => {
+    switch (category) {
+      case '자랑하개':
+        return '/img/img_bone_bowl.png'
+      case '영양있개':
+        return '/img/img_dog_medication.png'
+      default:
+        return '/img/img_main_house.png'
+    }
+  }
+
   return (
     <c.Container>
-      <img src="/img/img_dog_medication.png" alt="" />
+      <img src={setImageSrc(data.communityCategoryName)} alt="" />
       <c.Right>
         <c.Category>{data.communityCategoryName}</c.Category>
         <c.Title>{data.title}</c.Title>
