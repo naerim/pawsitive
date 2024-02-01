715bd5c842cb9869104d9465736559559334e312
naerim <naerim1119@gmail.com>
2024-02-02 01:45:03 +0900

    ✨ feat : 커퓨니티 리스트 페이지 카테고리 컴포넌트 생성

---
+++ frontend/src/components/CommunityList/CommunityCategorySection.tsx
@@ -0,0 +1,26 @@
+import * as c from '@src/components/style/CommunityCategorySectionStyle'
+
+const CommunityCategorySection = () => {
+  return (
+    <c.Container>
+      <c.Item>
+        <img src="" alt="" />
+        <span>인기글</span>
+      </c.Item>
+      <c.Item>
+        <img src="" alt="" />
+        <span>쇼핑하개</span>
+      </c.Item>
+      <c.Item>
+        <img src="" alt="" />
+        <span>밥주개</span>
+      </c.Item>
+      <c.Item>
+        <img src="" alt="" />
+        <span>건강하개</span>
+      </c.Item>
+    </c.Container>
+  )
+}
+
+export default CommunityCategorySection
