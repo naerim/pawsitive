006e14f3a1c78b7e7706d27213277ce844e02d4d
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-01 16:35:46 +0900

    🎨 style : 오늘의 질문 컴포넌트 수정

--- frontend/src/components/Home/ThirdStage/DailyQuestion.tsx
+++ frontend/src/components/Home/ThirdStage/DailyQuestion.tsx
@@ -2,10 +2,10 @@ import * as d from '@src/components/style/DailyQuestionStyle'
 
 const DailyQuestion = () => {
   return (
-    <d.Container>
-      <d.Letter src="img/image_letter.png" alt="Dog Bone" />
+    <d.Container onClick={handleContainerClick}>
+      <d.DogBone src="img/img_dog_bones.png" alt="Dog Bone" />
       <d.Title>오늘의 질문이 도착했어요</d.Title>
-      <d.Button>&gt;</d.Button>
+      <d.SubTitle>반려견에 한걸음 더 다가가요.</d.SubTitle>
     </d.Container>
   )
 }
