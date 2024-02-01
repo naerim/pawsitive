27e050d80f524b080427751c70913a728b595386
최성호 <chltjdgh3@naver.com>
2024-02-01 17:49:18 +0900

    ♻️ refactor : Community type 변경

--- frontend/src/types/components/CommunityType.ts
+++ frontend/src/types/components/CommunityType.ts
@@ -1,10 +1,9 @@
-export type CommunityItemType = {
+export type CommunityPopularType = {
   boardNo: number
   memberEmail: string
   memberName: string
   title: string
   content: string
-  images: string[]
   isPublic: boolean
   latitude: number
   longitude: number
@@ -12,6 +11,29 @@ export type CommunityItemType = {
   hit: number
   communityCategoryNo: number
   communityCategoryName: string
+  images: string[]
+}
+
+// 디테일은 이거
+export type CommunityItemType = {
+  board: CommunityPopularType
+  comments: {
+    boardNo: number
+    commentNo: number
+    memberEmail: string
+    memberName: string
+    content: string
+    createdAt: string
+  }
+}
+
+// 커뮤니티 리스트
+export type CommunityListType = {
+  content: CommunityPopularType[]
+  totalPages: number
+  number: number
+  previous: boolean
+  next: boolean
 }
 
 export type CategoryType = {
