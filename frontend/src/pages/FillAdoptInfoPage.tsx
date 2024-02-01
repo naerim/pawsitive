30658f03518c3a2288a6bef3c516dea3e242f33c
KINHYEONJI <hyeonji0718@gmail.com>
2024-02-02 00:50:13 +0900

    ✨ feat : 입양 정보를 등록하는 페이지 스켈레톤 생성

---
+++ frontend/src/pages/FillAdoptInfoPage.tsx
@@ -0,0 +1,7 @@
+import FillAdoptInfoContainer from '@src/container/FillAdoptInfoContainer'
+
+const FillAdoptInfoPage = () => {
+  return <FillAdoptInfoContainer />
+}
+
+export default FillAdoptInfoPage
