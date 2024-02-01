2ae8bd6a557810b4420fed803bab3b535f3c7ceb
naerim <naerim1119@gmail.com>
2024-02-02 01:45:03 +0900

    ✨ feat : 커퓨니티 리스트 페이지 카테고리 컴포넌트 생성

---
+++ frontend/src/components/style/CommunityCategorySectionStyle.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  margin: 20px 0;
+`
+
+export const Item = styled.div`
+  display: flex;
+  cursor: pointer;
+  justify-content: center;
+  font-size: 0.9em;
+  padding: 6px 12px;
+  border-radius: 28px;
+  margin-right: 6px;
+  border: 1px solid #eaeaea;
+  label {
+    font-weight: 500;
+    margin-left: 4px;
+  }
+`
