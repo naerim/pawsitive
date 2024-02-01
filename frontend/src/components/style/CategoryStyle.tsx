8da160e3a33fe9d11636bef1cb5e567020e98c07
최성호 <chltjdgh3@naver.com>
2024-02-02 03:00:34 +0900

    🎨 style : community category style 수정

---
+++ frontend/src/components/style/CategoryStyle.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0px 0px 10px 0px;
+`
+
+export const Container = styled.div``
+
+export const HeaderButton = styled.button`
+  height: 30px;
+  width: auto;
+  margin: 5px 0px 5px 5px;
+  padding: 0px 7px;
+  //padding-right: 7px;
+  color: rgba(0, 0, 0, 0.8);
+  font-weight: bold;
+`
+
+export const Button = styled.button`
+  height: 25px;
+  width: auto;
+  margin: 5px 0px 5px 5px;
+  //margin-bottom: 5px;
+  //margin-right: 5px;
+  padding: 0px 7px;
+  //padding-right: 7px;
+  color: rgba(0, 0, 0, 0.8);
+  font-weight: 650;
+  border-radius: 8px;
+`
