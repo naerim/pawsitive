6b73111a23127b5958ed917acbee0fe68e8e8cb4
최성호 <chltjdgh3@naver.com>
2024-02-02 11:07:46 +0900

    🎨 style : kakao map style 수정

--- frontend/src/components/style/KakaoMapStyle.tsx
+++ frontend/src/components/style/KakaoMapStyle.tsx
@@ -7,9 +7,9 @@ export const MapContainer = styled.div`
 
 export const Map = styled.div`
   width: auto;
-  height: 400px;
-  border: 3px solid #fbefe6;
-  border-radius: 40px;
+  height: 580px;
+  //border: 3px solid #fbefe6;
+  border-radius: 10px;
 `
 
 export const CurrentButton = styled.button`
@@ -19,15 +19,27 @@ export const CurrentButton = styled.button`
   z-index: 3;
   background-color: rgba(0, 0, 0, 0);
   font-size: 35px;
+  outline: none;
+
+  .img {
+    width: 50px;
+    height: auto;
+    margin-bottom: -10px;
+    opacity: 0.5;
+  }
+
+  &:focus {
+    outline: none; /* 포커스 시에도 테두리가 나타나지 않도록 설정합니다. */
+  }
 `
 
 export const InfoWindow = styled.div`
   width: auto;
   height: 100px;
-  margin-top: 10px;
-  margin-bottom: 10px;
+  //margin-top: 10px;
+  //margin-bottom: 10px;
   padding: 10px;
-  background-color: #fde2ca;
+  background-color: #fffaf7;
   border-radius: 10px;
   color: #ff984f;
 `
