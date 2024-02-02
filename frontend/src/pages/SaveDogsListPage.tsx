abb593e5818355db2981cb71bcc80fa4c849e90a
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-02 10:06:48 +0900

    ✨ feat	: 관심 유기견 리스트 페이지 skeleton 생성

---
+++ frontend/src/pages/SaveDogsListPage.tsx
@@ -0,0 +1,7 @@
+import SaveDogsListContainer from '@src/container/SaveDogsListContainer'
+
+const SaveDogsListPage = () => {
+  return <SaveDogsListContainer />
+}
+
+export default SaveDogsListPage
