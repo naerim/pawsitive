7ea1877b7b9afa8bc35792a01411ca5d0f449e30
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-02 10:08:26 +0900

    ✨ feat	: 채팅 시작을 유도하는 RecommendChatCard 컴포넌트 생성

--- frontend/src/components/Home/SecondStage/index.tsx
+++ frontend/src/components/Home/SecondStage/index.tsx
@@ -1,11 +1,13 @@
 import HomeProgressBar from '@src/components/Home/HomeProgressBar'
 import HomeStatistics from '@src/components/Home/HomeStatistics'
 import HomeDictionary from '@src/components/Home/SecondStage/HomeDictionary'
-import HomeCommunityCard from '@src/components/Home/SecondStage/HomeCommunityCard.tsx'
+import HomeCommunityCard from '@src/components/Home/SecondStage/HomeCommunityCard'
+import RecommendChatCard from '@src/components/Home/SecondStage/RecommendChatCard'
 
 const Index = () => {
   return (
     <div>
+      <RecommendChatCard />
       <HomeProgressBar currentStage={2} />
       <HomeDictionary />
       <HomeStatistics />
