7905fb1382915599c7a01bae6b781695bbf7c3e7
이하늬 <109622@naver.com>
2024-02-04 04:25:24 +0900

    🐛 fix : 채팅 설정 변경

---
+++ backend/src/main/java/com/pawsitive/chatgroup/handler/StompEventListener.java
@@ -0,0 +1,37 @@
+package com.pawsitive.chatgroup.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+/**
+ * @author 이하늬
+ * @since 1.0
+ */
+
+@Component
+@Slf4j
+public class StompEventListener {
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionConnectedEvent event) {
+
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+
+        log.info("[Connected] websocket session id : {}", sessionId);
+    }
+
+    @EventListener
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String sessionId = headerAccessor.getSessionId();
+
+        log.info("[Disconnected] websocket session id : {}", sessionId);
+    }
+
+}
