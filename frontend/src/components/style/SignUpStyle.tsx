b6e460a4f210dd3eac8a5b5455529ba2b97ab3d1
이승현 <tmdgus1761@gmail.com>
2024-02-04 03:27:55 +0900

    🎨 style : 회원가입 페이지 style 적용

---
+++ frontend/src/components/style/SignUpStyle.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+export const InputContainer = styled.div`
+  margin-bottom: 16px;
+
+  h3 {
+    margin-bottom: 10px;
+  }
+
+  label {
+    display: block;
+    margin-bottom: 30px;
+
+    input {
+      margin-right: 5px;
+    }
+  }
+`
+
+export const InputLabel = styled.label`
+  display: block;
+  margin-bottom: 8px;
+  font-size: 18px;
+  color: #333;
+`
+
+export const InputField = styled.input`
+  width: 100%;
+  padding: 8px;
+  font-size: 16px;
+  border: none;
+  border-bottom: 1px solid #ccc;
+  outline: none;
+
+  &:focus {
+    border-bottom-color: #007bff;
+  }
+
+  &::placeholder {
+    color: #999;
+  }
+`
