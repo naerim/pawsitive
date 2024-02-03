a6aa686ce595668ab8305a1408e1b14fb96909ee
이승현 <tmdgus1761@gmail.com>
2024-02-04 03:16:49 +0900

    ✨ feat : 회원가입 관련 params, response type 수정

--- frontend/src/types/userType.ts
+++ frontend/src/types/userType.ts
@@ -17,19 +17,24 @@ export type AfterAdoptionUserInfo = {
 }
 
 export type JoinUserType = {
-  email: string
-  password: string
   name: string
-  address: string
   role: string
+  email: string
+  pw: string
+  birth: string
+  gender: string
+  address: string
+  type: number
 }
 
 export type JoinUserResponseType = {
-  address: string
-  email: string
-  image: string
-  name: string
-  password: string
-  role: string
   userNo: number
+  email: string
+  name: string
+  address: string
+  role: string
+  birth: string
+  gender: string
+  type: number
+  stage: number
 }
