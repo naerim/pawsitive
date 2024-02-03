ca17b78bc92fba85f860b8b02c226005b8fc2862
최성호 <chltjdgh3@naver.com>
2024-02-04 00:47:07 +0900

    ✨ feat : login user api 호출

--- frontend/src/apis/user.ts
+++ frontend/src/apis/user.ts
@@ -1,12 +1,62 @@
 import { publicRequest } from '@src/hooks/requestMethods'
-import { JoinUserResponseType, JoinUserType } from '@src/types/userType'
+import {
+  JoinUserResponseType,
+  JoinUserType,
+  LoginUserResponseType,
+  LoginUserType,
+} from '@src/types/userType'
+import axios from 'axios'
 
 export const fetchAfterAdoptionUser = async () => {
-  return publicRequest.get('api/v1/users/admin/dogs').then(res => res.data)
+  return publicRequest.get('users/admin/dogs').then(res => res.data)
 }
 
 export const joinUser = async (
   userData: JoinUserType,
 ): Promise<JoinUserResponseType> => {
-  return publicRequest.post('api/v1/auth/join', userData).then(res => res.data)
+  return publicRequest.post('auth/join', userData).then(res => res.data)
 }
+
+// 로그인 api
+export const loginUser = async (
+  loginData: LoginUserType,
+): Promise<LoginUserResponseType> => {
+  return publicRequest
+    .post('auth/login', loginData)
+    .then(res => {
+      const { accessToken, grantType } = res.data
+      axios.defaults.headers.common.Authorization = `${grantType} ${accessToken}`
+      return res.data
+    })
+    .catch(error => {
+      console.log(error)
+      throw new Error('로그인 에러')
+    })
+}
+
+// const JWT_EXPIRY_TIME = 24 * 3600 * 1000
+// export const onLoginSuccess = async (res: AxiosResponse) => {
+//   const { accessToken, grantType } = res.data
+//   axios.defaults.headers.common.Authorization = `${grantType} ${accessToken}`
+//   setTimeout(onSilentRefresh, JWT_EXPIRY_TIME - 60000)
+// }
+//
+// export const loginUser = async (loginData: LoginUserType): Promise<void> => {
+//   return publicRequest
+//     .post('auth/login', loginData)
+//     .then(onLoginSuccess)
+//     .catch(error => {
+//       console.log(error)
+//       throw new Error('로그인 에러')
+//     })
+// }
+//
+// export const onSilentRefresh = async () => {
+//   return publicRequest
+//       .post('/silent-refresh')
+//       .then(onLoginSuccess)
+//       .catch(error => {
+//         console.log(error)
+//         throw new Error('리프레시 토큰 연장 에러')
+//       })
+// }
