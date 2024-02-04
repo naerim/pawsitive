692f95c20fc0ad43b54fb3a22bb027df6a81a3cd
이승현 <tmdgus1761@gmail.com>
2024-02-05 01:45:41 +0900

    ✨ feat : 인증 메일 요청 및 인증 코드 검정 api 기능 구현

--- frontend/src/apis/user.ts
+++ frontend/src/apis/user.ts
@@ -1,5 +1,7 @@
 import { publicRequest } from '@src/hooks/requestMethods'
 import {
+  EmailCodeVerifyResponseType,
+  EmailCodeVerifyType,
   JoinUserResponseType,
   JoinUserType,
   LoginUserResponseType,
@@ -11,10 +13,27 @@ export const fetchAfterAdoptionUser = async () => {
   return publicRequest.get('users/admin/dogs').then(res => res.data)
 }
 
+// 회원가입
 export const joinUser = async (
   userData: JoinUserType,
 ): Promise<JoinUserResponseType> => {
   return publicRequest.post('/auth/join', userData).then(res => res.data)
+}
+
+// 인증 메일 요청
+export const fetchEmailVerification = async (email: string) => {
+  return publicRequest
+    .get(`/auth/email/verify?email=${email}`)
+    .then(res => res.data)
+}
+
+// 인증 코드 검증
+export const verifyEmailCode = async (
+  emailData: EmailCodeVerifyType,
+): Promise<EmailCodeVerifyResponseType> => {
+  return publicRequest
+    .post('/auth/email/verify', emailData)
+    .then(res => res.data)
 }
 
 // 로그인 api
