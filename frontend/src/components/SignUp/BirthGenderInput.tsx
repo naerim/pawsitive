dcd2da76a14ebd8ccc8780c02a6341ae120181cf
이승현 <tmdgus1761@gmail.com>
2024-02-05 00:03:53 +0900

    ✨ feat : 생년월일, 성별 유효성 검사 최종 수정

--- frontend/src/components/SignUp/BirthGenderInput.tsx
+++ frontend/src/components/SignUp/BirthGenderInput.tsx
@@ -4,79 +4,75 @@ import { signUpDataAtom, signUpErrorAtom } from '@src/stores/atoms/user'
 import * as s from '@src/components/style/SignUpStyle'
 
 const BirthGenderInput = () => {
-  const [signUpData, setSignUpData] = useAtom(signUpDataAtom)
+  const [, setSignUpData] = useAtom(signUpDataAtom)
   const [error, setError] = useAtom(signUpErrorAtom)
   const [inputValue, setInputValue] = useState('')
 
-  const convertToYYYYMMDD = (yyMMdd, genderInput) => {
-    const year = yyMMdd.slice(0, 2)
+  const determineCentury = (genderInput: string) => {
+    return genderInput === '1' || genderInput === '2' ? '19' : '20'
+  }
+  const convertToYYYYMMDD = (yyMMdd: string, genderInput: string) => {
+    const year = determineCentury(genderInput) + yyMMdd.slice(0, 2)
     const month = yyMMdd.slice(2, 4)
     const day = yyMMdd.slice(4, 6)
 
-    let century
-
-    if (genderInput === '1' || genderInput === '2') {
-      century = '19'
-    } else if (genderInput === '3' || genderInput === '4') {
-      century = '20'
-    }
-
-    return `${century}${year}-${month}-${day}`
+    return `${year}-${month}-${day}`
   }
-  const handleInputChange = e => {
+
+  const validateInput = (
+    yy: string,
+    mm: string,
+    dd: string,
+    genderInput: string,
+  ): boolean => {
+    const currentYear = new Date().getFullYear().toString().slice(2)
+    const currentDate = new Date()
+
+    return (
+      ![1, 2, 3, 4].includes(parseInt(genderInput, 10)) ||
+      new Date(convertToYYYYMMDD(`${yy}${mm}${dd}`, genderInput)) >
+        currentDate ||
+      parseInt(mm, 10) < 1 ||
+      parseInt(mm, 10) > 12 ||
+      parseInt(dd, 10) < 1 ||
+      parseInt(dd, 10) > 31 ||
+      (parseInt(yy, 10) > parseInt(currentYear, 10) &&
+        [3, 4].includes(parseInt(genderInput, 10)))
+    )
+  }
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target
 
-    if (!/^[0-9-]+$/.test(value)) {
-      setError(prevError => ({
-        ...prevError,
-        dob: '올바른 형식으로 입력하세요.',
-      }))
-    } else if (value.length === 7) {
-      const yy = value.slice(0, 2)
-      const mm = value.slice(2, 4)
-      const dd = value.slice(4, 6)
-      const genderInput = value.slice(6)
-
-      if (value[6] !== '-' || !/[1-4]/.test(genderInput)) {
-        setError(prevError => ({
-          ...prevError,
-          dob: '올바른 형식으로 입력하세요.',
-        }))
-      } else {
+    if (/^[0-9-]+$/.test(value) && value.length === 8 && value[6] === '-') {
+      const [yy, mm, dd, genderInput] = [
+        value.slice(0, 2),
+        value.slice(2, 4),
+        value.slice(4, 6),
+        value.slice(7),
+      ]
+      if (
+        /[1-4]/.test(genderInput) &&
+        !validateInput(yy, mm, dd, genderInput)
+      ) {
         const fullDate = convertToYYYYMMDD(value.replace('-', ''), genderInput)
-        const currentYear = new Date().getFullYear().toString().slice(2)
-        const currentDate = new Date()
 
-        if (
-          (yy > currentYear && [3, 4].includes(parseInt(genderInput, 10))) ||
-          ![1, 2, 3, 4].includes(parseInt(genderInput, 10)) ||
-          new Date(fullDate) > currentDate ||
-          mm < 1 ||
-          mm > 12 ||
-          dd < 1 ||
-          dd > 31
-        ) {
-          setError(prevError => ({
-            ...prevError,
-            dob: '올바른 형식으로 입력하세요.',
-          }))
-        } else {
-          setSignUpData(prevData => ({
-            ...prevData,
-            birth: fullDate,
-            gender: genderInput === '1' || genderInput === '3' ? 'M' : 'F',
-          }))
-          setInputValue(value)
-          setError(prevError => ({ ...prevError, dob: '' }))
-        }
+        setSignUpData(prevData => ({
+          ...prevData,
+          birth: fullDate,
+          gender: genderInput === '1' || genderInput === '3' ? 'M' : 'F',
+        }))
+        setInputValue(value)
+        setError(prevError => ({ ...prevError, dob: '' }))
+        return
       }
-    } else {
-      setInputValue(value)
-      setError(prevError => ({ ...prevError, dob: '' }))
     }
-  }
 
-  console.log(signUpData.birth, signUpData.gender)
+    setInputValue(value)
+    setError(prevError => ({
+      ...prevError,
+      dob: '올바른 형식으로 입력하세요.',
+    }))
+  }
 
   return (
     <s.InputContainer>
