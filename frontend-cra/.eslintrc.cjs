bb95472620c8da367d31b937d95345fb913664b9
naerim <naerim1119@gmail.com>
2024-02-04 13:59:01 +0900

    🌱 chore : prettier, eslint setting

---
+++ frontend-cra/.eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'serviceWorkerRegistration.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/function-component-definition': [
+      2,
+      { namedComponents: ['arrow-function', 'function-declaration'] },
+    ],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+  },
+}
---
+++ frontend-cra/src/container/ChattingListContainer.tsx
@@ -0,0 +1,9 @@
+const ChattingListContainer = () => {
+  return (
+    <div>
+      <div>채팅 리스트 페이지</div>
+    </div>
+  )
+}
+
+export default ChattingListContainer
