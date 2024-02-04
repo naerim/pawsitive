e25b20e4cb4837b03ed0c68e4f10989d764ce94f
naerim <naerim1119@gmail.com>
2024-02-04 17:41:24 +0900

    ⚙️ config : croco 절대 경로 설정

--- frontend-cra/src/App.tsx
+++ frontend-cra/src/App.tsx
@@ -3,15 +3,17 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { useAtomValue } from 'jotai'
-import { GlobalStyle } from './style/GlobalStyles'
-import { themeAtom } from './stores/theme'
-import { darkTheme, lightTheme } from './style/theme.'
-import ChatPage from './pages/Chat'
+import ChatPage from '@pages/Chat'
+import ChatRoomPage from '@pages/ChatRoom'
+import { themeAtom } from '@stores/theme'
+import { GlobalStyle } from '@style/GlobalStyles'
+import { darkTheme, lightTheme } from '@style/theme.'
 
 const AuthRoutes = () => (
   <Routes>
     <Route path="/" element={<ChatPage />} />
     <Route path="/chat" element={<ChatPage />} />
+    <Route path="/chat/:roomId" element={<ChatRoomPage />} />
   </Routes>
 )
 
