92e781b6183f62ac154e8f8f16024716c90eea2b
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-05 23:50:31 +0900

    🎨 style : 이미지가 없을 경우에도 컴포넌트 크기가 고정되도록 style 수정

--- frontend/src/common/BasicDogInfoCard.tsx
+++ frontend/src/common/BasicDogInfoCard.tsx
@@ -4,7 +4,9 @@ const BasicDogInfoCard = () => {
   return (
     <b.Container>
       <b.AdoptStatus>공고중</b.AdoptStatus>
-      <b.Dogimg src="/img/img_dog1.png" />
+      <b.ImgContainer>
+        <b.Dogimg src="/img/img_dog1.png" />
+      </b.ImgContainer>
       <b.DogTextInfoContainer>
         <b.DogName>까미</b.DogName>
         <b.SubInfo>암컷 ∙ 중성화X</b.SubInfo>
