34c899a166acf9635764af5eb44b00b9bcdbabbb
최성호 <chltjdgh3@naver.com>
2024-02-05 21:25:34 +0900

    🎨 style : my page style 추가

--- frontend/src/components/style/MyPageMenuStyle.tsx
+++ frontend/src/components/style/MyPageMenuStyle.tsx
@@ -4,9 +4,48 @@ import { Link } from 'react-router-dom'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 0 20px;
 `
 
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  width: auto;
+  aspect-ratio: 1.6 / 1;
+  background-color: #fd9132;
+  color: white;
+`
+export const word = styled.div`
+  padding: 20px;
+`
+export const Pawsitiver = styled.p`
+  font-size: 15px;
+  margin-bottom: 10px;
+`
+export const Name = styled.div`
+  font-weight: 500;
+  font-size: 20px;
+`
+
+export const Img = styled.img`
+  align-self: flex-end;
+  width: 223px;
+  margin-right: -25px;
+`
+
+export const Body = styled.div``
+
+export const MenuItemDiv = styled.div`
+  margin: 30px 25px 30px 25px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 export const MenuItem = styled(Link)`
-  margin-bottom: 20px;
+  width: 100%;
+  font-size: 15px;
+  color: black;
+`
+export const RightArrow = styled.img`
+  width: 15px;
 `
