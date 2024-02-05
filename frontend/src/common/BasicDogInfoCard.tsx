a170568f834e0bc1b2d03cd1340d44dac569fcfc
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-05 15:45:35 +0900

    🎨 style : BasicDogInfoCard style 적용

--- frontend/src/common/BasicDogInfoCard.tsx
+++ frontend/src/common/BasicDogInfoCard.tsx
@@ -1,5 +1,17 @@
+import * as b from '@src/common/style/BasicDogInfoCardStyle'
+
 const BasicDogInfoCard = () => {
-  return <div />
+  return (
+    <b.Container>
+      <b.AdoptStatus>공고중</b.AdoptStatus>
+      <b.Dogimg src="/img/img_dog1.png" />
+      <b.DogTextInfoContainer>
+        <b.DogName>까미</b.DogName>
+        <b.SubInfo>암컷 ∙ 중성화X</b.SubInfo>
+        <b.SubInfo>2023(년생) ∙ 믹스</b.SubInfo>
+      </b.DogTextInfoContainer>
+    </b.Container>
+  )
 }
 
 export default BasicDogInfoCard
