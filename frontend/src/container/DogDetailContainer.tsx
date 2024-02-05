0b1c8574b038245e3bb49a28c9f1d18b6d554b01
김내림 <naerim1119@naver.com>
2024-02-05 17:01:52 +0900

    🎨 style : 보호소와 채팅하기 버튼 생성

--- frontend/src/container/DogDetailContainer.tsx
+++ frontend/src/container/DogDetailContainer.tsx
@@ -1,10 +1,21 @@
 import DogFileSection from '@src/components/DogDetail/DogFileSection'
+import ShelterName from '@src/components/DogDetail/ShelterName'
+import DogAdditionalInfo from '@src/components/DogDetail/DogAdditionalInfo'
+import ChatStartButton from '@src/components/DogDetail/ChatStartButton'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  padding-bottom: 80px;
+`
 
 const DogDetailContainer = () => {
   return (
-    <div>
+    <Container>
       <DogFileSection />
-    </div>
+      <ShelterName />
+      <DogAdditionalInfo />
+      <ChatStartButton />
+    </Container>
   )
 }
 
