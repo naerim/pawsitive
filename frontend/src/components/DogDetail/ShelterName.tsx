31df520b43be91cc3b194073de3e4f6c49dc502d
김내림 <naerim1119@naver.com>
2024-02-05 16:50:45 +0900

    🎨 style : 보호소 이름 및 정보

---
+++ frontend/src/components/DogDetail/ShelterName.tsx
@@ -0,0 +1,15 @@
+import * as s from '@src/components/DogDetail/_style/ShelterNameStyle'
+
+const ShelterName = () => {
+  return (
+    <s.Container>
+      <s.Circle />
+      <s.Right>
+        <s.Title>광주유기견보호소</s.Title>
+        <s.Address>광주광역시</s.Address>
+      </s.Right>
+    </s.Container>
+  )
+}
+
+export default ShelterName
