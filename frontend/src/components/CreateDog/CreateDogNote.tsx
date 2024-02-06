cd37127b4abea77170f6d98dc45159836ea213ad
이승현 <tmdgus1761@gmail.com>
2024-02-06 11:11:30 +0900

    ✨ feat : 유기견 등록 기본 정보 컴포넌트 atom 적용

---
+++ frontend/src/components/CreateDog/CreateDogNote.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useAtom } from 'jotai'
+import { createDogInfoAtom } from '@src/stores/atoms/dog'
+import * as c from '@src/components/style/CreateDogNoteStyle'
+
+const CreateDogNote = () => {
+  const [, setCreateDogData] = useAtom(createDogInfoAtom)
+
+  const handleNoteChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const noteInput = e.target.value
+    setCreateDogData(prevData => ({ ...prevData, note: noteInput }))
+  }
+
+  return (
+    <c.Container>
+      <div>추가정보</div>
+      <c.CustomTextarea
+        onChange={handleNoteChange}
+        placeholder="성격, 보호 과정 등 강아지의 특징을 적어주세요!"
+      />
+    </c.Container>
+  )
+}
+
+export default CreateDogNote
