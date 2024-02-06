b273dce9ec175fdaf9ef65536fbc9654c3696a5b
이승현 <tmdgus1761@gmail.com>
2024-02-06 14:17:20 +0900

    ✨ feat : 유기견 등록 추가 정보 입력갑 value 설정

--- frontend/src/components/CreateDog/CreateDogNote.tsx
+++ frontend/src/components/CreateDog/CreateDogNote.tsx
@@ -4,7 +4,7 @@ import { createDogInfoAtom } from '@src/stores/atoms/dog'
 import * as c from '@src/components/style/CreateDogNoteStyle'
 
 const CreateDogNote = () => {
-  const [, setCreateDogData] = useAtom(createDogInfoAtom)
+  const [createDogData, setCreateDogData] = useAtom(createDogInfoAtom)
 
   const handleNoteChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const noteInput = e.target.value
@@ -15,6 +15,7 @@ const CreateDogNote = () => {
     <c.Container>
       <div>추가정보</div>
       <c.CustomTextarea
+        value={createDogData.note}
         onChange={handleNoteChange}
         placeholder="성격, 보호 과정 등 강아지의 특징을 적어주세요!"
       />
