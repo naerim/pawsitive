8a0c0d3c3dced70238d2f89fa0709141afcc1d4f
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-06 14:26:41 +0900

    🎨 style : 무한 스크롤으로 인해 container height가 계속 바뀜에 따라 %가 아닌 px로 단위 변경

--- frontend/src/components/style/DogListContainer.tsx
+++ frontend/src/components/style/DogListContainer.tsx
@@ -2,11 +2,12 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
   width: 90%;
-  margin: 0 auto 8vh auto;
+  margin: 0 auto 70px auto;
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(48%, 1fr));
-  gap: 1%;
-  padding: 3% 0 20% 0;
+  gap: 3px;
+  padding: 3px 0;
+  height: fit-content;
 `
 
 export const FakeDiv = styled.div`
