70009216f7874f6895c012c4af871c908dcfa418
이승현 <tmdgus1761@gmail.com>
2024-02-06 11:13:32 +0900

    ✨ feat : 유기견 등록 파일 업로드 컴포넌트 수정

--- frontend/src/components/CreateDog/CreateDogFile.tsx
+++ frontend/src/components/CreateDog/CreateDogFile.tsx
@@ -1,6 +1,6 @@
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import { CreateDogFileType } from '@src/types/components/CreateDogType'
-import * as c from '@src/components/style/CreateDogFileStyle'
+import * as c from '@src/components/style/CommunityCreateFormStyle'
 
 const CreateDogFile = (props: CreateDogFileType) => {
   const { file, setFile } = props
@@ -65,8 +65,8 @@ const CreateDogFile = (props: CreateDogFileType) => {
                 </div>
               ))}
           </div>
-          <c.SelectFile htmlFor="file">파일찾기</c.SelectFile>
-          <c.FileInput
+          <label htmlFor="file">파일찾기</label>
+          <input
             id="file"
             name="files"
             type="file"
@@ -83,6 +83,85 @@ const CreateDogFile = (props: CreateDogFileType) => {
       )}
     </div>
   )
+
+  // const { file, setFile } = props
+  // const [, setNameList] = useState<File[] | null>(null)
+  //
+  // // const [imageViewValue] = useState<string[]>([])
+  //
+  // // const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+  // //   const { files } = e.target
+  // //   let imageUrlLists: string[] = [...createDogData.images]
+  // //   if (files) {
+  // //     const fileArray = Array.from(files, f => f as File)
+  // //     setCreateDogData(prevData => ({
+  // //       ...prevData,
+  // //       images: [
+  // //         ...prevData.images,
+  // //         ...fileArray.map(file => URL.createObjectURL(file)),
+  // //       ],
+  // //     }))
+  // //     for (let i = 0; i < files.length; i += 1) {
+  // //       const currentImageUrl = URL.createObjectURL(files[i])
+  // //       imageUrlLists.push(currentImageUrl)
+  // //     }
+  // //     if (imageUrlLists.length > 10) {
+  // //       imageUrlLists = imageUrlLists.slice(0, 10)
+  // //     }
+  // //     setImageView(imageUrlLists)
+  // //   }
+  // // }
+  //
+  // // const handleDeleteImage = (id: number) => {
+  // //   setImageView(imageViewValue.filter((_, index) => index !== id))
+  // //   setFile(prevData => ({
+  // //     ...prevData,
+  // //     images: prevData.images.filter((_, index) => index !== id),
+  // //   }))
+  // // }
+  //
+  // const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
+  //   const { files } = e.target
+  //   if (files) {
+  //     const fileArray = Array.from(files, f => f as File) // Use a mapping function
+  //     const count = fileArray.filter(f => f.type.includes('video')).length
+  //
+  //     if (count <= 1) {
+  //       setFile(fileArray)
+  //       setNameList(fileArray)
+  //     } else {
+  //       alert('동영상은 1개만 첨부 가능합니다.')
+  //     }
+  //   }
+  // }
+  //
+  // return (
+  //   <c.ImgContainer>
+  //     <c.ImgLabel htmlFor="image">
+  //       <c.ImgBox>
+  //         <img className="img" src="/icon/icon_camera.png" alt="" />
+  //         <p className="p">{file.length}/10</p>
+  //       </c.ImgBox>
+  //     </c.ImgLabel>
+  //     <c.ImageInput
+  //       id="image"
+  //       type="file"
+  //       accept="image/*"
+  //       multiple
+  //       onChange={handleFileUpload}
+  //     />
+  //     {imageViewValue.map((imageFile, id) => (
+  //       <c.ImagePreview key={`${imageFile}`}>
+  //         <img
+  //           alt={`${imageFile}-${id}`}
+  //           src={imageFile}
+  //           style={{ height: '70px', width: '70px' }}
+  //         />
+  //         {/* <c.Delete onClick={() => handleDeleteImage(id)}>X</c.Delete> */}
+  //       </c.ImagePreview>
+  //     ))}
+  //   </c.ImgContainer>
+  // )
 }
 
 export default CreateDogFile
