228cdedc59f63dcad0e3f83d82376ce471965127
김내림 <naerim1119@naver.com>
2024-02-06 11:38:01 +0900

    🎨 style : 커뮤니티 header style 수정

---
+++ frontend/src/container/style/CommunityListInfoContainerStyle.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+  margin: 20px auto 0 auto;
+`
+
+export const Container = styled.div``
+
+export const HeaderButton = styled.button`
+  width: auto;
+  font-weight: 500;
+  background-color: white;
+  border: none;
+
+  &:focus {
+    outline: none; /* 포커스 시에도 테두리가 나타나지 않도록 설정합니다. */
+  }
+`
+
+export const CreateButton = styled.button`
+  color: #ff9232;
+`
