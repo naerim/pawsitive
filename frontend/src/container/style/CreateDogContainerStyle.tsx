0ad3307027f4789f5d795060c7de42f1e33d8e63
이승현 <tmdgus1761@gmail.com>
2024-02-06 11:08:00 +0900

    🎨 style : 유기견 등록 단계별 container style 적용

--- frontend/src/container/style/CreateDogContainerStyle.tsx
+++ frontend/src/container/style/CreateDogContainerStyle.tsx
@@ -1,6 +1,31 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  padding-bottom: 60px;
-  background-color: aliceblue;
+  margin: 40px 20px 0;
+  padding: 10px 20px 60px;
+`
+
+export const TopContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 50px;
+  font-size: 15px;
+  font-weight: 500;
+`
+
+export const Button = styled.button`
+  background-color: #ff9232;
+  color: #fff;
+  padding: 10px 0;
+  width: 100%;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 60px;
+
+  &:disabled {
+    background-color: #bebebe;
+    cursor: not-allowed;
+  }
 `
