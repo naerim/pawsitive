208dcd2aa8ed453af8f118be39633761e96acba3
천세진 <sejinnnnnn@naver.com>
2024-02-06 21:42:09 +0900

    🐛 fix : DogStatusEnum 수정

--- backend/src/main/java/com/pawsitive/doggroup/dogenum/DogStatusEnum.java
+++ backend/src/main/java/com/pawsitive/doggroup/dogenum/DogStatusEnum.java
@@ -4,7 +4,36 @@ import lombok.Getter;
 
 @Getter
 public enum DogStatusEnum {
-    TODO,
-    INPROGRESS,
-    DONE
+    TODO("공고 중", 0),
+    INPROGRESS("절차 진행 중", 1),
+    DONE("입양 완료", 2);
+
+    private String name;
+    private Integer no;
+
+    DogStatusEnum(String name, int no) {
+        this.name = name;
+        this.no = no;
+    }
+
+    public static String IntToString(int n) {
+        for (DogStatusEnum value : DogStatusEnum.values()) {
+            if (value.getNo() == n) {
+                return value.getName();
+            }
+        }
+
+        return null;
+    }
+
+    public static DogStatusEnum intToEnum(int n) {
+        for (DogStatusEnum value : DogStatusEnum.values()) {
+            if (value.getNo() == n) {
+                return value;
+            }
+        }
+
+        return null;
+    }
+
 }
