94aebf58a1e8811d75e4225193c467190b15bee7
최성호 <chltjdgh3@naver.com>
2024-02-07 00:13:36 +0900

    ✨ feat : question create 생성

---
+++ frontend/src/pages/QuestionCreatePage.tsx
@@ -0,0 +1,11 @@
+import QuestionCreateContainer from '@src/container/QuestionCreateContainer'
+
+const QuestionCreatePage = () => {
+  return (
+    <div>
+      <QuestionCreateContainer />
+    </div>
+  )
+}
+
+export default QuestionCreatePage
