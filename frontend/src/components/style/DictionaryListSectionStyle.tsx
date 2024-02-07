2936468a409d94b0128ab246d2ac7b960c17db43
이승현 <tmdgus1761@gmail.com>
2024-02-07 15:58:28 +0900

    🎨 style : 펫과사전 전체 조회 페이지 style 적용

---
+++ frontend/src/components/style/DictionaryListSectionStyle.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  height: 100%;
+  margin-top: 10px;
+`
+export const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Box = styled.div`
+  height: 40px;
+`
