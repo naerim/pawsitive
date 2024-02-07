e0822876ec9fc42fa5470b320d27e106145a4b59
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-07 16:22:53 +0900

    ✨ feat	: 이미 채팅을 한 적이 있다면 기존 채팅방으로 이동

--- frontend/src/components/DogDetail/ChatStartButton.tsx
+++ frontend/src/components/DogDetail/ChatStartButton.tsx
@@ -5,11 +5,12 @@ import { fetchChatRooms } from '@src/apis/chat'
 import { ChatRoomType } from '@src/types/chatType'
 import { useAtom } from 'jotai/index'
 import { userAtom } from '@src/stores/atoms/user'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 const ChatStartButton = () => {
   const location = useLocation()
   const dogNo = location.state?.dogNo
+  const navigate = useNavigate()
   const [chatRooms, setChatRooms] = useState<ChatRoomType[]>([])
   const [user] = useAtom(userAtom)
 
@@ -24,9 +25,21 @@ const ChatStartButton = () => {
     }
   }, [isLoading, data, user.userNo, dogNo])
 
+  const doesChatRoomExist = () => {
+    return chatRooms.some(chatRoom => chatRoom.dogNo === dogNo)
+  }
+
+  const handleClick = () => {
+    if (doesChatRoomExist()) {
+      navigate('/chatroom')
+    }
+  }
+
   return (
     <a.Container>
-      <a.Button type="button">보호소와 채팅하기</a.Button>
+      <a.Button type="button" onClick={handleClick}>
+        보호소와 채팅하기
+      </a.Button>
     </a.Container>
   )
 }
