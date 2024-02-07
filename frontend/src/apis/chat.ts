253e7a70aa0a2041c447749443a078473fd3ce10
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-07 16:12:52 +0900

    ✨ feat	: 채팅방을 생성하는  createChatRoom 함수 생성

--- frontend/src/apis/chat.ts
+++ frontend/src/apis/chat.ts
@@ -1,9 +1,17 @@
 import { publicRequest } from '@src/hooks/requestMethods'
-import { ChatRoomType } from '@src/types/chatType' // 현재 사용자 채팅 리스트 전체 조회
+import { ChatRoomType, CreateChatRoomParamsType } from '@src/types/chatType' // 현재 사용자 채팅 리스트 전체 조회
 
 // 현재 사용자 채팅 리스트 전체 조회
 export const fetchChatRooms = async (
   userNo: number,
 ): Promise<ChatRoomType[]> => {
   return publicRequest.get(`/chatrooms?userNo=${userNo}`).then(res => res.data)
+}
+
+// 채팅방 생성
+export const createChatRoom = async (
+  chatRoomParams: CreateChatRoomParamsType,
+): Promise<ChatRoomType> => {
+  console.log(chatRoomParams)
+  return publicRequest.post(`/chatrooms`, chatRoomParams).then(res => res.data)
 }
