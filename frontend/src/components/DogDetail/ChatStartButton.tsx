619642b474f3b325c0176810ff9e8e39d0e1c426
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-07 16:20:22 +0900

    ✨ feat	: 상세 페이지가 렌더링 될 때, 전체 채팅방 조회 설정

--- frontend/src/components/DogDetail/ChatStartButton.tsx
+++ frontend/src/components/DogDetail/ChatStartButton.tsx
@@ -1,8 +1,28 @@
 import * as a from '@src/components/DogDetail/_style/ChatStartButtonStyle'
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { fetchChatRooms } from '@src/apis/chat'
+import { ChatRoomType } from '@src/types/chatType'
+import { useAtom } from 'jotai/index'
+import { userAtom } from '@src/stores/atoms/user'
+import { useLocation } from 'react-router-dom'
 
-const ChatStartButton: React.FC<{ dogNo: number }> = ({ dogNo }) => {
-  console.log(dogNo)
+const ChatStartButton = () => {
+  const location = useLocation()
+  const dogNo = location.state?.dogNo
+  const [chatRooms, setChatRooms] = useState<ChatRoomType[]>([])
+  const [user] = useAtom(userAtom)
+
+  const { data, isLoading } = useQuery<ChatRoomType[]>({
+    queryKey: ['fetchChatRooms'],
+    queryFn: () => fetchChatRooms(user.userNo),
+  })
+
+  useEffect(() => {
+    if (!isLoading && data) {
+      setChatRooms(data)
+    }
+  }, [isLoading, data, user.userNo, dogNo])
 
   return (
     <a.Container>
