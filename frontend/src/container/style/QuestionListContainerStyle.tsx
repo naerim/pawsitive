71f1674994421423ca20144af4b73e722ff01623
이승현 <tmdgus1761@gmail.com>
2024-02-08 12:13:32 +0900

    🎨 style : 오늘의 질문 전체 리스트 조회 컨테이너 style 수정

---
+++ frontend/src/container/style/QuestionListContainerStyle.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 90%;
+  display: flex;
+  flex-direction: column;
+  gap: 80px;
+  padding: 15px;
+  margin: 40px auto;
+`
