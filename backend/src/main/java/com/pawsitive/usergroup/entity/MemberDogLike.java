0bd7321b0db576535c8d1f279f48cc27f4e602f2
천세진 <sejinnnnnn@naver.com>
2024-02-09 00:17:48 +0900

    ✨ feat : 조회 / 찜 테이블 분리 + 엔티티 수정

---
+++ backend/src/main/java/com/pawsitive/usergroup/entity/MemberDogLike.java
@@ -0,0 +1,42 @@
+package com.pawsitive.usergroup.entity;
+
+import com.pawsitive.doggroup.entity.Dog;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "member_dog_like")
+public class MemberDogLike {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_dog_like_no")
+    private int memberDogNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_no")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "dog_no")
+    private Dog dog;
+
+    @Column(name = "created_at", insertable = false)
+    private LocalDateTime createdAt;
+}
