9ea6a50ab55c8fbdcd721a34bfa752c1ed883dd7
naerim <naerim1119@gmail.com>
2024-02-09 02:53:14 +0900

    🌱 chore : 채팅 목록에서 채팅방 생성 버튼 삭제

--- frontend/src/container/ChattingContainer.tsx
+++ frontend/src/container/ChattingContainer.tsx
@@ -1,8 +1,8 @@
 import { useAtom } from 'jotai/index'
 import { userAtom } from '@src/stores/atoms/user'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import { ChatRoomType } from '@src/types/chatType'
-import { createChatRoom, fetchChatRooms } from '@src/apis/chat'
+import { fetchChatRooms } from '@src/apis/chat'
 import { useNavigate } from 'react-router-dom'
 
 const ChattingContainer = () => {
@@ -14,27 +14,17 @@ const ChattingContainer = () => {
     queryFn: () => fetchChatRooms(user.userNo),
   })
 
-  const { mutate } = useMutation({
-    mutationKey: ['createChatRoom'],
-    mutationFn: createChatRoom,
-  })
-
-  const createRoomButton = () => mutate({ userNo: user.userNo, dogNo: 6 })
-
   const goChatRoom = (id: string) => navigate(`/chat/${id}`)
 
   return (
     <div>
       <p>{user.name}님의 채팅방 목록</p>
-      <button type="button" onClick={createRoomButton}>
-        채팅방 만들기
-      </button>
       {!isLoading &&
         data &&
         data.map(item => (
           <div
             key={item.chatRoomNo}
-            style={{ marginBottom: 40, backgroundColor: 'red' }}
+            style={{ marginBottom: 40, backgroundColor: 'skyblue' }}
             onClick={() => goChatRoom(item.chatRoomNo)}
           >
             <p>{item.chatRoomNo}번 방</p>
