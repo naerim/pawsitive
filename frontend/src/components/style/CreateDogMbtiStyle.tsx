8440be0b967b03aa28b31ec034748a0b31d1777b
이승현 <tmdgus1761@gmail.com>
2024-02-08 10:17:39 +0900

    🎨 style : 유기견 등록 mbti 컴포넌트 style 수정

--- frontend/src/components/style/CreateDogMbtiStyle.tsx
+++ frontend/src/components/style/CreateDogMbtiStyle.tsx
@@ -1,13 +1,34 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-bottom: 30px;
+`
 
-export const Title = styled.h1`
-  font-size: 15px;
+export const Title = styled.div`
+  display: block;
+  font-size: 14px;
   font-weight: 500;
-  margin-bottom: 10px;
+  color: #333;
+`
+
+export const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `
 
 export const RadioWrap = styled.div`
-  padding: 10px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  font-size: 14px;
+  padding: 5px 0;
+`
+export const QuesContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 4px;
 `
