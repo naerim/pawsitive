f9fde10f302080ad7f0c71074d250fbcd2b7e4c7
최성호 <chltjdgh3@naver.com>
2024-02-09 02:44:16 +0900

    ✨ feat : adoption survey type 수정

---
+++ frontend/src/types/surveyType.ts
@@ -0,0 +1,22 @@
+export type SurveyFormType = {
+  No1: string
+  No2: string
+  No3: string
+  No4: string
+  No5: string
+  No6: boolean
+  No7: boolean
+  No8: string
+  No9: string
+  No10: string
+  No11: string
+  No12: string
+  No13: boolean
+  No14: string
+  No15: string
+  No16: boolean
+  No17: string
+  No18: string
+  No19: string
+  No20: boolean
+}
