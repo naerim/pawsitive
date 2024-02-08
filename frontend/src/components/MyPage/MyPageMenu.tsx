055fccf960d316b86e5fa100836b7ff3c786d5c9
최성호 <chltjdgh3@naver.com>
2024-02-09 03:39:08 +0900

    ♻️ refactor : 이미지 참조 형식 변경

--- frontend/src/components/MyPage/MyPageMenu.tsx
+++ frontend/src/components/MyPage/MyPageMenu.tsx
@@ -13,44 +13,44 @@ const MyPageMenu = () => {
           <m.Pawsitiver>포지티버</m.Pawsitiver>
           <m.Name>{user.name ? `${user.name}님` : '사용자 이름'}</m.Name>
         </m.word>
-        <m.Img src="/public/img/img_dog_bones.png" />
+        <m.Img src="/img/img_dog_bones.png" />
       </m.Header>
       <m.Body>
         <m.MenuItem to="/mypage/profile">
           <m.MenuItemDiv>
             회원정보 수정
-            <m.RightArrow src="/public/icon/icon_black_arrow_right.png" />
+            <m.RightArrow src="/icon/icon_black_arrow_right.png" />
           </m.MenuItemDiv>
         </m.MenuItem>
 
         <m.MenuItem to="/mypage/setting">
           <m.MenuItemDiv>
             환경 설정
-            <m.RightArrow src="/public/icon/icon_black_arrow_right.png" />
+            <m.RightArrow src="/icon/icon_black_arrow_right.png" />
           </m.MenuItemDiv>
         </m.MenuItem>
         <m.MenuItem to="/mypage/findSimilarDog">
           <m.MenuItemDiv>
             나와 닮은꼴 강아지 찾기
-            <m.RightArrow src="/public/icon/icon_black_arrow_right.png" />
+            <m.RightArrow src="/icon/icon_black_arrow_right.png" />
           </m.MenuItemDiv>
         </m.MenuItem>
         <m.MenuItem to="/mypage/survey">
           <m.MenuItemDiv>
             입양설문 하러가기
-            <m.RightArrow src="/public/icon/icon_black_arrow_right.png" />
+            <m.RightArrow src="/icon/icon_black_arrow_right.png" />
           </m.MenuItemDiv>
         </m.MenuItem>
         <m.MenuItem to="/login">
           <m.MenuItemDiv>
             로그인(임시)
-            <m.RightArrow src="/public/icon/icon_black_arrow_right.png" />
+            <m.RightArrow src="/icon/icon_black_arrow_right.png" />
           </m.MenuItemDiv>
         </m.MenuItem>
 
         <m.MenuItemDiv>
           <LogoutModal />
-          <m.RightArrow src="/public/icon/icon_black_arrow_right.png" />
+          <m.RightArrow src="/icon/icon_black_arrow_right.png" />
         </m.MenuItemDiv>
       </m.Body>
     </m.Container>
