8570ce29cc35e3831e71f82375a1cdd78d74e0d5
KINHYEONJI <hyeonji0718@gmail.com>
2024-02-10 00:49:22 +0900

    ✨ feat : 보호소일 경우 체크리스트 안내 렌더링 되지 않도록 설정

--- frontend/src/components/Home/DefaultStage/index.tsx
+++ frontend/src/components/Home/DefaultStage/index.tsx
@@ -3,13 +3,17 @@ import HomeStatistics from '@src/components/Home/HomeStatistics'
 import ShowDogCard from '@src/components/Home/DefaultStage/ShowDogCard'
 
 const Index = () => {
+  const userRole = JSON.parse(window.localStorage.getItem('currentUser')).role
+
   return (
     <div>
-      <MainColorMoveCard
-        title="포지티버가 되어 볼까요?"
-        subTitle="체크리스트 확인 후 강아지들을 만나봐요"
-        url="/confirm/pawsitive"
-      />
+      {userRole === 'USER' && (
+        <MainColorMoveCard
+          title="포지티버가 되어 볼까요?"
+          subTitle="체크리스트 확인 후 강아지들을 만나봐요"
+          url="/confirm/pawsitive"
+        />
+      )}
       <HomeStatistics />
       <ShowDogCard />
     </div>
