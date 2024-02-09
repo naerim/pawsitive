a6dc6ca2e9d9053842929fde6ff3f2f71682ee40
천세진 <sejinnnnnn@naver.com>
2024-02-09 18:31:46 +0900

    🐛 fix : DogKindEnum 추가 및 순서 수정

--- backend/src/main/java/com/pawsitive/doggroup/dogenum/DogKindEnum.java
+++ backend/src/main/java/com/pawsitive/doggroup/dogenum/DogKindEnum.java
@@ -4,11 +4,23 @@ import lombok.Getter;
 
 @Getter
 public enum DogKindEnum {
-    BICHON("비숑"), POMERANIAN("포메라니안"), POODLE("푸들");
-    private final String name;
+    MALTESE("말티즈", 0),
+    BICHON("비숑", 1),
+    CHIHUAHUA("치와와", 2),
+    POODLE("푸들", 3),
+    POMERANIAN("포메라니안", 4),
+    SHIBA("시바견", 5),
+    SHIHTZU("시츄", 6),
+    DOBERMAN("도베르만", 7),
+    RETRIEVER("리트리버", 8),
+    ETC("기타", 9);
 
-    DogKindEnum(String name) {
+    private final String name;
+    private final Integer no;
+
+    DogKindEnum(String name, Integer no) {
         this.name = name;
+        this.no = no;
     }
 
     public static DogKindEnum stringToEnum(String s) {
