194adea66f3397900c19df4c3d578c1d91d0d5cc
KINHYEONJI <hyeonji0718@gmail.com>
2024-02-09 22:06:26 +0900

    🐛 fix : 채팅 시작 전 전체 조회 후 채팅방 여부 확인 로직 삭제

--- frontend/src/components/DogDetail/ChatStartButton.tsx
+++ frontend/src/components/DogDetail/ChatStartButton.tsx
@@ -1,8 +1,7 @@
 import * as a from '@src/components/DogDetail/_style/ChatStartButtonStyle'
 import { useEffect, useState } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { createChatRoom, fetchChatRooms } from '@src/apis/chat'
-import { ChatRoomType, CreateChatRoomParamsType } from '@src/types/chatType'
+import { createChatRoom } from '@src/apis/chat'
+import { CreateChatRoomParamsType } from '@src/types/chatType'
 import { useAtom } from 'jotai/index'
 import { userAtom } from '@src/stores/atoms/user'
 import { useLocation, useNavigate } from 'react-router-dom'
@@ -11,27 +10,13 @@ const ChatStartButton = () => {
   const location = useLocation()
   const dogNo = location.state?.dogNo
   const navigate = useNavigate()
-  const [chatRooms, setChatRooms] = useState<ChatRoomType[]>([])
   const [user] = useAtom(userAtom)
   const [createChatRoomParams, setCreateChatRoomParams] =
     useState<CreateChatRoomParamsType>()
 
-  const { data, isLoading } = useQuery<ChatRoomType[]>({
-    queryKey: ['fetchChatRooms'],
-    queryFn: () => fetchChatRooms(user.userNo),
-  })
-
   useEffect(() => {
-    if (!isLoading && data) {
-      setChatRooms(data)
-      setCreateChatRoomParams({ dogNo, userNo: user.userNo })
-      console.log(data)
-    }
-  }, [isLoading, data, user.userNo, dogNo])
-
-  const findChatRoomIndex = () => {
-    return chatRooms.findIndex(chatRoom => chatRoom.dogNo === dogNo)
-  }
+    setCreateChatRoomParams({ dogNo, userNo: user.userNo })
+  }, [dogNo, user])
 
   const handleClick = async () => {
     const currentChatRoomIndex = findChatRoomIndex()
