de580c3cb3af0fc32ee838f4e4c5a60bda2e839c
천세진 <sejinnnnnn@naver.com>
2024-02-09 19:25:24 +0900

    ✨ feat : Entity 별 repository 생성

---
+++ backend/src/main/java/com/pawsitive/usergroup/repository/MemberDogLikeRepository.java
@@ -0,0 +1,7 @@
+package com.pawsitive.usergroup.repository;
+
+import com.pawsitive.usergroup.entity.MemberDogLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberDogLikeRepository extends JpaRepository<MemberDogLike, Long> {
+}
