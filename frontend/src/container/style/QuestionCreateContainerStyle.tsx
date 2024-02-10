527394c15a9bdb783f9376bc927fb86eea6efff4
이승현 <tmdgus1761@gmail.com>
2024-02-10 16:06:25 +0900

    ✨ feat : 오늘의 질문 답변 작성 페이지 뒤로가기 기능 구현

--- frontend/src/container/style/QuestionCreateContainerStyle.tsx
+++ frontend/src/container/style/QuestionCreateContainerStyle.tsx
@@ -8,3 +8,33 @@ export const Container = styled.div`
   padding: 15px;
   margin: 40px auto;
 `
+
+export const TopContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 30px;
+  font-size: 15px;
+  font-weight: 500;
+  position: relative;
+`
+
+export const BackButtonWrap = styled.div`
+  cursor: pointer;
+
+  img {
+    width: 9px;
+    height: 15px;
+  }
+`
+
+export const Span = styled.div`
+  width: 9px;
+  height: 10px;
+`
+
+export const Title = styled.div`
+  text-align: center;
+  font-size: 1em;
+  font-weight: 500;
+`
