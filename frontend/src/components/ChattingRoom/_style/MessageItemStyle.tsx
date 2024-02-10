d39c47f6cc87954975833e0846780b0ab53e8746
naerim <naerim1119@gmail.com>
2024-02-10 18:28:00 +0900

    ✨ feat : 채팅 보낸 시간 추가

--- frontend/src/components/ChattingRoom/_style/MessageItemStyle.tsx
+++ frontend/src/components/ChattingRoom/_style/MessageItemStyle.tsx
@@ -4,15 +4,31 @@ export const Container = styled.div`
   display: flex;
 `
 
+export const Wrap = styled.div<{ $me: boolean }>`
+  display: flex;
+  margin-left: ${props => props.$me && 'auto'};
+  flex-direction: ${props => props.$me && 'row-reverse'};
+  max-width: 90%;
+  margin-bottom: 14px;
+
+  span {
+    font-size: 0.7em;
+    margin-top: auto;
+    padding-bottom: 2px;
+    margin-left: ${props => !props.$me && '4px'};
+    margin-right: ${props => props.$me && '4px'};
+    color: #555;
+  }
+`
+
 export const MessageBox = styled.div<{ $me: boolean }>`
   display: flex;
   padding: 6px 10px;
   border-radius: 10px;
-  margin-bottom: 10px;
+
   background-color: ${props => (props.$me ? '#FF9232' : '#F2F3F6')};
   color: ${props => (props.$me ? '#fff' : '#000')};
   justify-content: center;
-  margin-left: ${props => props.$me && 'auto'};
-  max-width: 85%;
   word-break: break-word;
+  line-height: 1.2;
 `
