4bf2c4e7d01947fb5883396f45025c72df4c578b
이승현 <tmdgus1761@gmail.com>
2024-02-10 16:29:18 +0900

    ✨ feat : 펫과사전 전체 조회 페이지 뒤로가기 기능 구현

--- frontend/src/container/style/DictionaryListContainerStyle.tsx
+++ frontend/src/container/style/DictionaryListContainerStyle.tsx
@@ -1,14 +1,10 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
-
-export const Top = styled.div`
-  margin: 50px 0 30px;
-
-  p {
-    font-weight: 500;
-    text-align: center;
-  }
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+  margin: 40px auto;
 `
 
 export const Wrap = styled.div`
@@ -17,4 +13,34 @@ export const Wrap = styled.div`
   width: 86%;
   margin: 0 auto;
   padding-bottom: 100px;
+`
+
+export const TopContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 30px;
+  font-size: 15px;
+  font-weight: 500;
+  position: relative;
+`
+
+export const BackButtonWrap = styled.div`
+  cursor: pointer;
+
+  img {
+    width: 9px;
+    height: 15px;
+  }
+`
+
+export const Span = styled.div`
+  width: 9px;
+  height: 10px;
+`
+
+export const Title = styled.div`
+  text-align: center;
+  font-size: 1em;
+  font-weight: 500;
 `
