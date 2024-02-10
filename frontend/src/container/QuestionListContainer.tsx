eb70968e37ba8eb9bd1b197a2ab91f4990916acc
이승현 <tmdgus1761@gmail.com>
2024-02-10 16:05:57 +0900

    ✨ feat : 오늘의 질문 전체 조회 페이지 뒤로가기 기능 구현

--- frontend/src/container/QuestionListContainer.tsx
+++ frontend/src/container/QuestionListContainer.tsx
@@ -1,10 +1,23 @@
+import { useNavigate } from 'react-router-dom'
 import QuestionListSection from '@src/components/Question/QuestionListSection'
 import * as q from '@src/container/style/QuestionListContainerStyle'
 
 const QuestionListContainer = () => {
+  const navigate = useNavigate()
+
+  const handlePrevStep = () => {
+    navigate('/')
+  }
+
   return (
     <q.Container>
-      <div>오늘의 질문</div>
+      <q.TopContainer>
+        <q.BackButtonWrap onClick={handlePrevStep}>
+          <img src="/icon/icon_gray_arrow_left.png" alt="" />
+        </q.BackButtonWrap>
+        <q.Title>오늘의 질문</q.Title>
+        <q.Span />
+      </q.TopContainer>
       <QuestionListSection />
     </q.Container>
   )
