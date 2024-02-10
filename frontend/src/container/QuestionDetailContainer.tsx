0b25a11c855104e718c2bf19523ea00247bcae34
이승현 <tmdgus1761@gmail.com>
2024-02-10 16:06:10 +0900

    ✨ feat : 오늘의 질문 상세 조회 페이지 뒤로가기 기능 구현

--- frontend/src/container/QuestionDetailContainer.tsx
+++ frontend/src/container/QuestionDetailContainer.tsx
@@ -1,10 +1,23 @@
+import { useNavigate } from 'react-router-dom'
 import QuestionDetail from '@src/components/Question/QuestionDetail'
 import * as q from '@src/container/style/QuestionDetailContainerStyle'
 
 const QuestionDetailContainer = () => {
+  const navigate = useNavigate()
+
+  const handlePrevStep = () => {
+    navigate('/questions')
+  }
+
   return (
     <q.Container>
-      <div>오늘의 질문</div>
+      <q.TopContainer>
+        <q.BackButtonWrap onClick={handlePrevStep}>
+          <img src="/icon/icon_gray_arrow_left.png" alt="" />
+        </q.BackButtonWrap>
+        <q.Title>오늘의 질문</q.Title>
+        <q.Span />
+      </q.TopContainer>
       <QuestionDetail />
     </q.Container>
   )
