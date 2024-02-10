517071f40eeb1262fe07855b783c80ce50b4086f
이승현 <tmdgus1761@gmail.com>
2024-02-10 16:28:42 +0900

    🐛 fix : 펫과사전 props type 에러 해결

--- frontend/src/components/Dictionary/DictionaryListSection.tsx
+++ frontend/src/components/Dictionary/DictionaryListSection.tsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom'
-import { DictionaryItemType } from '@src/types/components/DictionaryType'
 import DictionaryListItem from '@src/components/Dictionary/DictionaryListItem'
 import * as c from '@src/components/style/DictionaryListSectionStyle'
+import { DictionaryItemType } from '@src/types/components/DictionaryType'
 
-const DictionaryListSection = (props: { data: DictionaryItemType }) => {
+const DictionaryListSection = (props: { data: DictionaryItemType[] }) => {
   const { data } = props
 
   return (
