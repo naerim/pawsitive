e35550fc8312488fe4d225ab5d3b9e1ad869b17c
최성호 <chltjdgh3@naver.com>
2024-02-11 17:00:01 +0900

    🎨 style : 입양한 강아지 정보 카드 style 수정

--- frontend/src/components/style/AdoptInfoStyle.tsx
+++ frontend/src/components/style/AdoptInfoStyle.tsx
@@ -1,15 +1,53 @@
 import styled from 'styled-components'
 
+export const Flip = styled.div``
 export const Container = styled.div`
   width: 188px;
   height: 225px;
-  border-radius: 9px;
-  background: #ff9232;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  margin: 0 5px 0 0;
+  // z축에 깊이 주기 위해
+  perspective: 1100px;
+`
+
+export const Card = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+  // 입체감 주기 위해
+  transition: 0.2s;
+  transform-style: preserve-3d;
+
+  &:hover {
+    transform: rotateY(180deg);
+  }
+`
+export const FrontCard = styled.div`
+  width: 188px;
+  height: 225px;
+  position: absolute;
   padding: 11px;
   margin: 2px;
+  border-radius: 9px;
+  background: #ff9232;
+  backface-visibility: hidden;
+`
+export const BackCard = styled.div`
+  width: 188px;
+  height: 225px;
+  margin: 0 0 0 5px;
+  // 앞, 뒤 카드 같은 위치에 둠
+  position: absolute;
+  padding: 11px;
+  margin: 2px;
+  border-radius: 9px;
+  background: #ff9232;
+  //  앞면에 반전된 뒷 면의 글들을 투영시키지 않기 위해
+  backface-visibility: hidden;
+  // 뒤집힌 상태로 둠
+  transform: rotateY(180deg);
 `
 
 export const DogImage = styled.img`
@@ -23,6 +61,7 @@ export const TextContainer = styled.div`
   align-items: center;
   justify-content: space-between;
   width: 100%;
+  margin-top: 5px;
 `
 
 export const TogetherContainer = styled.div`
@@ -48,4 +87,19 @@ export const Day = styled.div`
   font-size: 19px;
   font-weight: 500;
   line-height: normal;
+`
+
+export const Text = styled.div`
+  margin: 10px;
+  font-size: 18px;
+  font-weight: 400;
+  color: #fff;
+`
+
+export const ModButton = styled.button`
+  margin: 5px 10px 10px 10px;
+  padding: 2px 5px;
+  color: #f59021;
+  font-weight: 600;
+  background-color: white;
 `
