34b786e485d597ce551bbad25f536da91ae9c95e
naerim <naerim1119@gmail.com>
2024-02-11 12:32:55 +0900

    🎨 style : 펫과사전 이미지, 텍스트 잘리는 부분 수정

--- frontend/src/components/style/DictionaryListItemStyle.tsx
+++ frontend/src/components/style/DictionaryListItemStyle.tsx
@@ -4,10 +4,10 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  margin-bottom: 10px;
-  padding: 6px;
+  margin-bottom: 14px;
   border: 1px solid #d7d7d7;
   border-radius: 10px;
+  overflow: hidden;
 `
 
 export const Left = styled.div`
@@ -19,19 +19,22 @@ export const Left = styled.div`
 export const Category = styled.div`
   color: #ff9232;
   font-weight: 500;
-  font-size: 0.7em;
+  font-size: 0.8em;
 `
 
 export const Title = styled.div`
   color: #000;
   font-size: 1em;
   font-weight: 500;
-  line-height: 1.5;
-  margin: 2px 0;
+  margin: 6px 0;
+  width: 210px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
 `
 
 export const Image = styled.img`
   width: 80px;
   height: 80px;
-  padding: 6px;
+  transform: translate(2px, 10px) scale(1.3);
 `
