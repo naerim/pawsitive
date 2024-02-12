219d7ccc613259683bbbc45c8445fe778e9a4d58
KINHYEONJI <hyeonji0718@gmail.com>
2024-02-13 00:50:51 +0900

    ✨ feat : 보호소의 경우 공고리스트 상단에 유기견 등록 기능 style 적용

--- frontend/src/common/style/TextHeaderStyle.tsx
+++ frontend/src/common/style/TextHeaderStyle.tsx
@@ -8,14 +8,22 @@ export const Container = styled.div`
   height: 60px;
   z-index: 100;
   background-color: #fff;
+  padding: 0 5%;
 `
 
 export const Wrap = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  width: 90%;
+  width: 100%;
   margin: 0 auto;
   font-weight: 500;
   font-size: 1.1em;
+`
+
+export const PlusButton = styled.div`
+  font-size: 14px;
+  font-weight: 400;
+  padding: 3% 0 3% 3%;
+  cursor: pointer;
 `
