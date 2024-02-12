b3b36277dc15df1fdaca64206184fb27a781d22c
KINHYEONJI <hyeonji0718@gmail.com>
2024-02-12 14:50:43 +0900

    🚚 rename : 다양하게 쓰이는 ChattingListHeader를 TextHeader로 이름 변경
    - ChattingListHeader -> TextHeader

--- frontend/src/container/DogListContainer.tsx
+++ frontend/src/container/DogListContainer.tsx
@@ -7,7 +7,7 @@ import { fetchBasicDogList } from '@src/apis/dog'
 import Filter from '@src/components/DogList/Filter'
 import { useAtom } from 'jotai'
 import { dogListParamsAtom } from '@src/stores/atoms/dog'
-import ChattingListHeader from '@src/common/ChattingListHeader'
+import TextHeader from '@src/common/TextHeader'
 
 const DogListContainer = () => {
   const [basicDogListParams, setBasicDogListParams] = useAtom(dogListParamsAtom)
@@ -81,7 +81,7 @@ const DogListContainer = () => {
 
   return (
     <d.Container>
-      <ChattingListHeader title="유기견 공고 리스트" />
+      <TextHeader title="유기견 공고 리스트" />
       <d.FilterContainer>
         <d.ShowFilterButton type="button" onClick={showFilterHandle}>
           필터링
