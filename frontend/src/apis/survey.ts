883bc359f0a1bc365ee541767d148ae047faf9fa
최성호 <chltjdgh3@naver.com>
2024-02-12 16:56:49 +0900

    ✨ feat : 입양 설문 조회 페이지 api 연결

--- frontend/src/apis/survey.ts
+++ frontend/src/apis/survey.ts
@@ -12,3 +12,13 @@ export const fetchSurveySubmit = (
       throw new Error('입양 설문 등록 에러')
     })
 }
+
+export const fetchSurveyGet = (num: number) => {
+  return publicRequest
+    .get(`/users/survey?userNo=${num}`)
+    .then(res => res.data)
+    .catch(error => {
+      console.log(error)
+      throw new Error('유저 입양 설문 조회 에러')
+    })
+}
