a152bd7161707f4e450502acc1050c4733569e97
최성호 <chltjdgh3@naver.com>
2024-02-12 16:21:24 +0900

    ♻️ refactor: 로컬스토리지 비어있으면 에러떠서 임시로 수정

--- frontend/src/components/Home/DefaultStage/index.tsx
+++ frontend/src/components/Home/DefaultStage/index.tsx
@@ -1,9 +1,16 @@
 import MainColorMoveCard from '@src/common/MainColorMoveCard'
 import HomeStatistics from '@src/components/Home/HomeStatistics'
 import ShowDogCard from '@src/components/Home/DefaultStage/ShowDogCard'
+import { useEffect, useState } from 'react'
 
 const Index = () => {
-  const userRole = JSON.parse(window.localStorage.getItem('currentUser')).role
+  const [userRole, setUserRole] = useState('')
+  // 로그인 안한 상태로, 로컬스토리지 비어있으면 에러떠서 임시로 수정해두었습니다.
+  useEffect(() => {
+    if (window.localStorage.getItem('currentUser')) {
+      setUserRole(JSON.parse(window.localStorage.getItem('currentUser')).role)
+    }
+  }, [])
 
   return (
     <div>
