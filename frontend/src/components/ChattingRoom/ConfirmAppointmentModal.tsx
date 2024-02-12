03c18a9d90e65056a63810b6fd920d0f428ca1be
naerim <naerim1119@gmail.com>
2024-02-13 01:05:42 +0900

    ✨ feat : 입양약속 정보 연결

--- frontend/src/components/ChattingRoom/ConfirmAppointmentModal.tsx
+++ frontend/src/components/ChattingRoom/ConfirmAppointmentModal.tsx
@@ -2,27 +2,30 @@ import XModal from '@src/common/XModal'
 import * as c from '@src/components/ChattingRoom/_style/ConfirmAppointmentModalStyle'
 import { useAtom } from 'jotai'
 import { userAtom } from '@src/stores/atoms/user'
-import { CloseFunctionType } from '@src/types/commonType'
+import { ConfirmAppointmentModalType } from '@src/types/appointment'
 
-const ConfirmAppointmentModal = (props: CloseFunctionType) => {
-  const { onClose } = props
+const ConfirmAppointmentModal = (props: ConfirmAppointmentModalType) => {
+  const { onClose, chatRoomNo, dogName, shelterName, memberName, promise } =
+    props
   const [user] = useAtom(userAtom)
 
   return (
     <XModal onClose={onClose}>
-      <c.State>수락완료</c.State>
-      <c.Title>올림보호소와 입양약속</c.Title>
+      <c.State>{promise.isAccepted ? '수락완료' : '대기중'}</c.State>
+      <c.Title>
+        {user.role === 'USER' ? shelterName : memberName}님과 입양약속
+      </c.Title>
       <c.Row>
         <div>이름</div>
-        <div>참참이</div>
+        <div>{dogName}</div>
       </c.Row>
       <c.Row>
         <div>날짜</div>
-        <div>2024.01.23</div>
+        <div>{promise.date}</div>
       </c.Row>
       <c.Row>
         <div>시간</div>
-        <div>오후 2시</div>
+        <div>{promise.time}</div>
       </c.Row>
       <c.BottomButtonWrap>
         {user.role === 'SHELTER' ? (
