d1976b105e5be922dee8c97b82ea41d10b1b6a4c
김내림 <naerim1119@naver.com>
2024-02-14 01:54:21 +0900

    ✨ feat : 화상통화 screen 생성

---
+++ frontend/src/components/ChattingRoom/_style/WebcamScreenStyle.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 0;
+  height: 100vh;
+  z-index: 100;
+  background-color: #fff;
+  width: 390px;
+`
+
+export const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+`
