f8cadc34fc2ba793b7e310707aa85f3cf1c6f738
KIMHYEONJI <hyeonji0718@gmail.com>
2024-02-14 00:01:58 +0900

    ✨ feat	: 입양 절차 안내 페이지 스켈레톤 생성

---
+++ frontend/src/pages/AdoptProgressInfoPage.tsx
@@ -0,0 +1,7 @@
+import AdoptProgressInfoContainer from '@src/container/AdoptProgressInfoContainer'
+
+const AdoptProgressInfoPage = () => {
+  return <AdoptProgressInfoContainer />
+}
+
+export default AdoptProgressInfoPage
