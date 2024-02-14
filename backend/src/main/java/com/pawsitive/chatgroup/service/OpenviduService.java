cc2eb961f38c7573bc18fa08c6638093c3baba07
이하늬 <109622@naver.com>
2024-02-14 16:25:57 +0900

    ♻️ refactor : 채팅 로직 변경

--- backend/src/main/java/com/pawsitive/chatgroup/service/OpenviduService.java
+++ backend/src/main/java/com/pawsitive/chatgroup/service/OpenviduService.java
@@ -1,20 +1,21 @@
 package com.pawsitive.chatgroup.service;
 
+import com.pawsitive.chatgroup.dto.request.SessionCreateReq;
 import com.pawsitive.chatgroup.dto.response.ChatSessionRes;
 import com.pawsitive.chatgroup.dto.response.ChatTokenRes;
 import io.openvidu.java.client.OpenViduHttpException;
 import io.openvidu.java.client.OpenViduJavaClientException;
-import java.util.Map;
 
 /**
  * @author 이하늬
  * @since 1.0
  */
 public interface OpenviduService {
-    ChatSessionRes createSessions(Map<String, Object> params)
+    ChatSessionRes createSessions(/*Map<String, Object> params, */
+        SessionCreateReq sessionCreateReq)
         throws OpenViduJavaClientException, OpenViduHttpException;
 
-    ChatTokenRes getToken(String sessionId, Map<String, Object> params)
+    ChatTokenRes getToken(String sessionId)
         throws OpenViduJavaClientException, OpenViduHttpException;
 
     String disconnectSessions(String sessionId)
