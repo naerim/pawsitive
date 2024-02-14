2382373eeb0302e2b4cca9c7ce129342255453df
이승현 <tmdgus1761@gmail.com>
2024-02-14 16:24:06 +0900

    🎨 style : 찜한 유기견 공고 페이지 상단 타이틀 style 수정

--- frontend/src/container/style/SaveDogListContainerStyle.tsx
+++ frontend/src/container/style/SaveDogListContainerStyle.tsx
@@ -3,7 +3,38 @@ import styled from 'styled-components'
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 100px;
-  margin: 0 auto;
+  gap: 80px;
+  margin: 20px auto 0;
   padding: 20px;
+`
+
+export const TopContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 15px;
+  font-weight: 500;
+  position: relative;
+  margin: 0 10px;
+`
+
+export const BackButtonWrap = styled.div`
+  cursor: pointer;
+
+  img {
+    width: 9px;
+    height: 15px;
+  }
+`
+
+export const Span = styled.div`
+  width: 9px;
+  height: 10px;
+`
+
+export const Title = styled.div`
+  text-align: center;
+  font-size: 1em;
+  font-weight: 500;
 `
