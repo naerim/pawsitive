9799b8202a993b1b3da179a711d61307ac267d02
천세진 <sejinnnnnn@naver.com>
2024-02-14 23:20:36 +0900

    🐛 fix : 보호소 조회 핫픽스

--- backend/src/main/java/com/pawsitive/usergroup/repository/MemberDogLikeRepositoryImpl.java
+++ backend/src/main/java/com/pawsitive/usergroup/repository/MemberDogLikeRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pawsitive.usergroup.repository;
 
 import com.pawsitive.usergroup.entity.MemberDogLike;
 import com.pawsitive.usergroup.entity.QMemberDogLike;
+import com.pawsitive.usergroup.entity.QUser;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 
@@ -10,6 +11,7 @@ import java.util.List;
 public class MemberDogLikeRepositoryImpl extends QuerydslRepositorySupport implements MemberDogLikeRepositoryCustom {
 
     private static final QMemberDogLike qMemberDogLike = QMemberDogLike.memberDogLike;
+    private static final QUser qUser = QUser.user;
 
     public MemberDogLikeRepositoryImpl() {
         super(MemberDogLike.class);
@@ -27,8 +29,9 @@ public class MemberDogLikeRepositoryImpl extends QuerydslRepositorySupport imple
     @Override
     public MemberDogLike getUserDogLiked(Integer dogNo, Integer userNo) {
         return from(qMemberDogLike)
+            .innerJoin(qMemberDogLike.member.user, qUser)
             .select(qMemberDogLike)
-            .where(qMemberDogLike.dog.dogNo.eq(dogNo), qMemberDogLike.member.userNo.eq(userNo))
+            .where(qMemberDogLike.dog.dogNo.eq(dogNo), qMemberDogLike.member.user.userNo.eq(userNo))
             .fetchOne();
     }
 
