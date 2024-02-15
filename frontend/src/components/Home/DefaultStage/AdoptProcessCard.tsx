ffab76cae73ce94840fbd60b496bc0c9451d634c
김내림 <naerim1119@naver.com>
2024-02-15 11:45:38 +0900

    🎨 style : home stage2 입양한 후기 이동 스타일 수정

--- frontend/src/components/Home/DefaultStage/AdoptProcessCard.tsx
+++ frontend/src/components/Home/DefaultStage/AdoptProcessCard.tsx
@@ -1,13 +1,16 @@
 import * as a from '@src/components/Home/_style/AdoptProcessCardStyle'
 import { useNavigate } from 'react-router-dom'
+import { useAtomValue } from 'jotai'
+import { userAtom } from '@src/stores/atoms/user'
 
 const AdoptProcessCard = () => {
+  const user = useAtomValue(userAtom)
   const navigate = useNavigate()
 
   const goAdoptProcessInfo = () => navigate('/adopt-process-info')
 
   return (
-    <a.Container onClick={goAdoptProcessInfo}>
+    <a.Container onClick={goAdoptProcessInfo} $state={user.role}>
       <a.Title>입양 단계 안내</a.Title>
       <a.ImageWrap>
         <img alt="arrow" src="/icon/icon_white_arrow_right.png" />
--- frontend/src/components/Home/SecondStage/HomeCommunityCard.tsx
+++ frontend/src/components/Home/SecondStage/HomeCommunityCard.tsx
@@ -19,7 +19,8 @@ const HomeCommunityCard = () => {
         <MainColorMoveCard
           title="유기견 입양한 후기 보러가요!"
           subTitle="포지티버의 후기 확인하기"
-          url="/community/map "
+          url="/community/map"
+          backgroundColor="#FF9232"
         />
       </h.Wrap>
     </h.Container>
