254c0449461098a01bc8da80951e9ea248035be6
최성호 <chltjdgh3@naver.com>
2024-02-15 11:40:47 +0900

    ♻️ refactor : 메인에서 추천 유기견 api 연결

--- frontend/src/components/Home/FirstStage/OndRecommendCard.tsx
+++ frontend/src/components/Home/FirstStage/OndRecommendCard.tsx
@@ -1,16 +1,37 @@
 import * as o from '@src/components/Home/_style/OndRecommendCardStyle'
 import { useAtomValue } from 'jotai'
 import { userAtom } from '@src/stores/atoms/user'
+import { useQuery } from '@tanstack/react-query'
+import { fetchRecommendDogCard } from '@src/apis/dog'
+import { useNavigate } from 'react-router-dom'
 
 const OndRecommendCard = () => {
   const user = useAtomValue(userAtom)
+  const navigate = useNavigate()
+  const { data, isLoading } = useQuery({
+    queryKey: ['recommendDogCard'],
+    queryFn: () => fetchRecommendDogCard(user.userNo),
+  })
+
   return (
-    <o.Container>
-      <o.SubTitle>유기견 공고 추천</o.SubTitle>
-      <o.Title>{user.name}님의 가족이 되고 싶어요</o.Title>
-      <o.Image src="img/img_dog1.png" alt="" />
-      <o.Button type="button">해당 공고 보러가기</o.Button>
-    </o.Container>
+    <div>
+      {!isLoading && data && (
+        <o.Container>
+          <o.SubTitle>유기견 공고 추천</o.SubTitle>
+          <o.Title>{user.name}님의 가족이 되고 싶어요</o.Title>
+          <o.Image src={data.file} alt="" />
+
+          <o.Button
+            type="button"
+            onClick={() => {
+              navigate(`/dogs/${data.dogNo}`)
+            }}
+          >
+            해당 공고 보러가기
+          </o.Button>
+        </o.Container>
+      )}
+    </div>
   )
 }
 
