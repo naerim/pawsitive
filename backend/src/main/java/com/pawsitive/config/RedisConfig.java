69cea5c14be8dba6d05deddc727a2d00b6fd75b3
이하늬 <109622@naver.com>
2024-02-16 06:52:24 +0900

    ✨ feat : 컨텐츠 캐싱 기능 추가

--- backend/src/main/java/com/pawsitive/config/RedisConfig.java
+++ backend/src/main/java/com/pawsitive/config/RedisConfig.java
@@ -10,6 +10,7 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -41,6 +42,9 @@ public class RedisConfig implements BeanClassLoaderAware {
 
         redisTemplate.setEnableTransactionSupport(true);
 
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
         return redisTemplate;
     }
 
@@ -52,5 +56,5 @@ public class RedisConfig implements BeanClassLoaderAware {
     public ClassLoader getClassLoader() {
         return classLoader;
     }
-    
+
 }
