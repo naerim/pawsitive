2f9282f8c87d87161d62a688f3c8896008389835
이승현 <tmdgus1761@gmail.com>
2024-02-15 13:42:29 +0900

    ✨ feat : 현재 진행중인 채팅방 목록 뒤로가기 기능 추가

--- frontend/src/common/TextHeader.tsx
+++ frontend/src/common/TextHeader.tsx
@@ -1,7 +1,7 @@
-import * as c from '@src/common/style/TextHeaderStyle'
 import { useAtom } from 'jotai'
 import { userAtom } from '@src/stores/atoms/user'
 import { useNavigate } from 'react-router-dom'
+import * as c from '@src/common/style/TextHeaderStyle'
 
 const TextHeader = (props: { title: string }) => {
   const { title } = props
@@ -12,14 +12,26 @@ const TextHeader = (props: { title: string }) => {
     navigate('/new/dog')
   }
 
+  const handlePrevStep = () => {
+    navigate('/')
+  }
+
   return (
     <c.Container>
-      <c.Wrap>
-        {title}
-        {user.role === 'SHELTER' && title === '유기견 공고' && (
+      {user.role === 'SHELTER' && title === '유기견 공고' ? (
+        <c.Wrap>
+          {title}
           <c.PlusButton onClick={handleButtonClick}>+ 등록</c.PlusButton>
-        )}
-      </c.Wrap>
+        </c.Wrap>
+      ) : (
+        <c.Wrap>
+          <c.BackButtonWrap onClick={handlePrevStep}>
+            <img src="/icon/icon_gray_arrow_left.png" alt="" />
+          </c.BackButtonWrap>
+          <c.Title>{title}</c.Title>
+          <c.Span />
+        </c.Wrap>
+      )}
     </c.Container>
   )
 }
